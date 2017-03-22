2b25d2ec8119ed6833db538d512f80454b12910a
Michael A. Amisano <mamisano@us.ibm.com>
2017-03-22 12:19:59 -0400

    re-write sendgrid_send routine

--- channel_output_email.py
+++ channel_output_email.py
@@ -7,6 +7,8 @@ import pprint
 from twisted.python import log
 import mandrill
 import requests
+import sendgrid
+from sendgrid.helpers.mail import *
 from htmlmin import minify
 from httpd_site import env
 from channel import OutputChannel
@@ -98,29 +100,6 @@ class EmailOutputChannel(OutputChannel):
         else:
             log.err("No email settings found")
             
-    def sendgrid_send(self, msg=None, canarydrop=None):
-		try:
-			url = 'https://api.sendgrid.com/v3/mail/send'
-			headers = {
-				'authorization': "Bearer ", settings.SENDGRID_API_KEY,
-				'content-type': "application/json"
-				}
-			payload = "{\"personalizations\":[{\"to\":[{\"email\":\""canarydrop['alert_email_recipient']"\",\"name\":\"\"}],\"subject\":\""msg['subject']"\"}],\"from\":{\"email\":\""address=msg['from_address']"\",\"name\":\""msg['from_display']"\"},\"subject\":\""msg['subject']"\",\"content\":[{\"type\":\"text/html\",\"value\":\""msg['body']"\"}],\"footer\":{\"enable\":true,\"text\":\"Thanks,/n The SendGrid Team\",\"html\":\"<p>Thanks</br>The SendGrid Team</p>\"},\"sandbox_mode\":{\"enable\":false}}"
-			
-			if settings.DEBUG:
-				pprint.pprint(payload)
-			else:
-				result = requests.request("POST", url, data=payload, headers=headers)
-				#Raise an error if the returned status is 4xx or 5xx
-                result.raise_for_status()
-
-            log.msg('Sent alert to {recipient} for token {token}'\
-                        .format(recipient=canarydrop['alert_email_recipient'],
-                                token=canarydrop.canarytoken.value()))
-
-        except requests.exceptions.HTTPError as e:
-            log.err('A sendgrid error occurred: %s - %s' % (e.__class__, e))
-
     def mailgun_send(self, msg=None, canarydrop=None):
         try:
             url = 'https://api.mailgun.net/v3/{}/messages'.format(settings.MAILGUN_DOMAIN_NAME)
@@ -178,3 +157,21 @@ class EmailOutputChannel(OutputChannel):
             # Mandrill errors are thrown as exceptions
             log.err('A mandrill error occurred: %s - %s' % (e.__class__, e))
             # A mandrill error occurred: <class 'mandrill.UnknownSubaccountError'> - No subaccount exists with the id 'customer-123'....
+
+	def sendgrid_send(self, msg=None, canarydrop=None):
+		try:
+			sg = sendgrid.SendGridAPIClient(apikey=settings.SENDGRID_API_KEY)
+			from_email = Email(msg['from_address'])
+			subject = msg['subject']
+			to_email = Email(canarydrop['alert_email_recipient'])
+			content = Content("text/plain", msg['body'])
+			message = Mail(from_email, subject, to_email, content)
+			
+			if settings.DEBUG:
+				pprint.pprint(mail)
+			else:
+				result = sg.client.mail.send.post(request_body=message.get())
+				
+			log.msg('Sent alert to {recipient} for token {token}'\
+                        .format(recipient=canarydrop['alert_email_recipient'],
+                                token=canarydrop.canarytoken.value()))
