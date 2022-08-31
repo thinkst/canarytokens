5918a21a5cb31e17ea34fe1015fc609d10e68579
Jacob Torrey <jacob@thinkst.com>
2022-08-31 15:48:47 -0600

    Fixed issues with msreg
    Signed-off-by: Jacob Torrey <jacob@thinkst.com>

--- msreg.py
+++ msreg.py
@@ -1,13 +1,4 @@
-import tempfile
-import shutil
-import datetime
-import random
-import base64
-from zipfile import ZipFile, ZipInfo
-from ziplib import MODE_DIRECTORY
 from cStringIO import StringIO
-
-import settings
 
 REG_TEMPLATE = """Windows Registry Editor Version 5.00
 
@@ -16,7 +7,7 @@ REG_TEMPLATE = """Windows Registry Editor Version 5.00
 
 [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\{PROCESS}]
 "ReportingMode"=dword:00000001
-"MonitorProcess"="cmd.exe /c start /min powershell.exe -windowstyle hidden -command \"&{Resolve-DnsName -Name \\\"$env:computername.$env:username.CMD.{TOKEN_DNS}}\\\"}\""
+"MonitorProcess"="cmd.exe /c start /min powershell.exe -windowstyle hidden -command \"&{{Resolve-DnsName -Name \\\"$env:computername.$env:username.CMD.{TOKEN_DNS}\\\"}}\""
 """
 
 def make_canary_msreg(url=None, process_name='klist.exe'):
@@ -24,8 +15,3 @@ def make_canary_msreg(url=None, process_name='klist.exe'):
     # TODO add registry file generation code
     return output_buf.getvalue()
 
-def format_time_for_doc(time):
-    return time.strftime('%Y-%m-%d')+'T'+ time.strftime('%H:%M:%S')+'Z'
-
-if __name__ == '__main__':
-    print REG_TEMPLATE.format(PROCESS='klist.exe', TOKEN_DNS='blah.canarytokens.com')
