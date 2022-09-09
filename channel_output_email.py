0f2b58654f9f2b16c8ea89e07776f3c13cc8cf00
Nicholas Rohrbeck <nick@thinkst.com>
2022-09-09 15:34:36 +0200

    Add protected get on data dict

--- channel_output_email.py
+++ channel_output_email.py
@@ -99,7 +99,7 @@ class EmailOutputChannel(OutputChannel):
         if 'cmd_computer_name' in self.data and 'cmd_user_name' in self.data:
             vars['CMDInformation'] = 'User {user} executed "{process}" on the host {computer}'.format(
                 user=self.data['cmd_user_name'],
-                process=self.data['cmd_process'],
+                process=self.data.get('cmd_process'),
                 computer=self.data['cmd_computer_name']
             )
 
