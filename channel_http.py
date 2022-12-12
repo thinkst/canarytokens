53428f801e291b7008df8dd94fd8e980168f83e8
Jacob Torrey <jacob@thinkst.com>
2022-12-12 16:56:23 -0700

    Commented out failing log call
    Signed-off-by: Jacob Torrey <jacob@thinkst.com>

--- channel_http.py
+++ channel_http.py
@@ -198,7 +198,8 @@ class CanarytokenPage(resource.Resource, InputChannel):
             return self.render_GET(request)
 
     def format_additional_data(self, **kwargs):
-        log.info(kwargs)
+        # TODO Figure out why this needed to be commented out?
+        #log.info(kwargs)
         additional_report = ''
         if kwargs.has_key('src_ip') and kwargs['src_ip']:
             additional_report += 'Source IP: {ip}'.format(ip=kwargs['src_ip'])
