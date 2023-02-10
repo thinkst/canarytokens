e3e5138c213aad8b3ed124fd721835d9c2e804fe
Emmanuel Pitswane <103185449+emmanuel-thinkst@users.noreply.github.com>
2023-02-10 11:15:54 +0200

    T5211 py3 port google chat webhook formatting (#184)
    * Add google chat api models
    
    * add google chat webhook formatting
    
    * improve webhook error handling
    
    * update webhook tests
    
    * Delete testdoc.xlsx
    
    ---------
    
    Co-authored-by: William Leighton Dawson <leighton@thinkst.com>

--- tests/units/test_channel_output_webhook.py
+++ tests/units/test_channel_output_webhook.py
@@ -2,9 +2,10 @@
 from twisted.logger import capturedLogs
 
 from canarytokens.canarydrop import Canarydrop
+from canarytokens.channel import format_as_googlechat_canaryalert
 from canarytokens.channel_dns import ChannelDNS
 from canarytokens.channel_output_webhook import WebhookOutputChannel
-from canarytokens.models import TokenTypes
+from canarytokens.models import TokenAlertDetailsGoogleChat, TokenTypes
 from canarytokens.settings import Settings
 from canarytokens.switchboard import Switchboard
 from canarytokens.tokens import Canarytoken
@@ -135,3 +136,120 @@ def test_broken_2_webhook(setup_db, webhook_receiver, settings: Settings):
     assert any(
         ["Failed sending request to webhook" in log["log_format"] for log in captured]
     )
+
+
+def test_googlechat_webhook_format(setup_db, webhook_receiver, settings: Settings):
+
+    switchboard = Switchboard()
+    input_channel = ChannelDNS(
+        switchboard=switchboard,
+        settings=settings,
+        backend_hostname="test.com",
+        backend_scheme="https",
+        listen_domain=settings.LISTEN_DOMAIN,
+    )
+
+    cd = Canarydrop(
+        type=TokenTypes.DNS,
+        generate=True,
+        alert_email_enabled=False,
+        alert_email_recipient="email@test.com",
+        alert_webhook_enabled=False,
+        alert_webhook_url=webhook_receiver,
+        canarytoken=Canarytoken(),
+        memo="memo",
+        browser_scanner_enabled=False,
+    )
+    token_hit = Canarytoken.create_token_hit(
+        token_type=TokenTypes.DNS,
+        input_channel="not_valid",
+        src_ip="127.0.0.1",
+        hit_info={"some": "data"},
+    )
+    cd.add_canarydrop_hit(token_hit=token_hit)
+    details = input_channel.gather_alert_details(
+        cd,
+        protocol=input_channel.backend_scheme,
+        host=input_channel.backend_hostname,
+    )
+    print("Webhook details = {}".format(details))
+    webhook_payload = format_as_googlechat_canaryalert(details=details)
+    webhook_payload_json = webhook_payload.json_safe_dict()
+    print("Webhook_payload json = {}".format(webhook_payload.json()))
+
+    assert "cardsV2" in webhook_payload_json.keys()
+    assert type(webhook_payload_json["cardsV2"]) is list
+    assert len(webhook_payload_json["cardsV2"]) == 1
+    for key in ["cardId", "card"]:
+        assert key in webhook_payload_json["cardsV2"][0].keys()
+    assert type(webhook_payload_json["cardsV2"][0]["cardId"]) is str
+    assert type(webhook_payload_json["cardsV2"][0]["card"]) is dict
+    for key in ["header", "sections"]:
+        assert key in webhook_payload_json["cardsV2"][0]["card"].keys()
+    assert type(webhook_payload_json["cardsV2"][0]["card"]["header"]) is dict
+    for key in ["title", "imageUrl", "imageType", "imageAltText"]:
+        assert key in webhook_payload_json["cardsV2"][0]["card"]["header"].keys()
+        assert type(webhook_payload_json["cardsV2"][0]["card"]["header"][key]) is str
+    assert type(webhook_payload_json["cardsV2"][0]["card"]["sections"]) is list
+    assert len(webhook_payload_json["cardsV2"][0]["card"]["sections"]) == 2
+    assert type(webhook_payload_json["cardsV2"][0]["card"]["sections"][0]) is dict
+    for key in ["header", "collapsible", "widgets"]:
+        assert key in webhook_payload_json["cardsV2"][0]["card"]["sections"][0].keys()
+    assert (
+        type(webhook_payload_json["cardsV2"][0]["card"]["sections"][0]["header"]) is str
+    )
+    assert (
+        type(webhook_payload_json["cardsV2"][0]["card"]["sections"][0]["collapsible"])
+        is bool
+    )
+    assert (
+        type(webhook_payload_json["cardsV2"][0]["card"]["sections"][0]["widgets"])
+        is list
+    )
+    assert type(webhook_payload_json["cardsV2"][0]["card"]["sections"][1]) is dict
+    for key in ["header", "collapsible", "widgets"]:
+        assert key in webhook_payload_json["cardsV2"][0]["card"]["sections"][1].keys()
+
+
+def test_canaryalert_googlechat_webhook(setup_db, webhook_receiver, settings: Settings):
+    """
+    Tests if a google chat webhook payload, is produced given a googlechat webhook receiver.
+    """
+    googlechat_webhook_receiver = (
+        "https://chat.googleapis.com/v1/spaces/random/messages?key=temp_key"
+    )
+
+    switchboard = Switchboard()
+    input_channel = ChannelDNS(
+        switchboard=switchboard,
+        settings=settings,
+        backend_hostname="test.com",
+        backend_scheme="https",
+        listen_domain=settings.LISTEN_DOMAIN,
+    )
+
+    cd = Canarydrop(
+        type=TokenTypes.DNS,
+        generate=True,
+        alert_email_enabled=False,
+        alert_email_recipient="email@test.com",
+        alert_webhook_enabled=False,
+        alert_webhook_url=googlechat_webhook_receiver,
+        canarytoken=Canarytoken(),
+        memo="memo",
+        browser_scanner_enabled=False,
+    )
+    token_hit = Canarytoken.create_token_hit(
+        token_type=TokenTypes.DNS,
+        input_channel="not_valid",
+        src_ip="127.0.0.1",
+        hit_info={"some": "data"},
+    )
+    cd.add_canarydrop_hit(token_hit=token_hit)
+
+    canaryalert_webhook_payload = input_channel.format_webhook_canaryalert(
+        canarydrop=cd,
+        protocol=input_channel.backend_scheme,
+        host=input_channel.backend_hostname,
+    )
+    assert isinstance(canaryalert_webhook_payload, TokenAlertDetailsGoogleChat)
