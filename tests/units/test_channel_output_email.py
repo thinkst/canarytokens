39ac47160822bea4c46c2df0ec477f7445b19ae0
W. Leighton Dawson <leighton@thinkst.com>
2023-06-17 20:58:33 +0200

    Remove temporary skip of sendgrid unit test

--- tests/units/test_channel_output_email.py
+++ tests/units/test_channel_output_email.py
@@ -2,7 +2,6 @@ import datetime
 from pathlib import Path
 
 from pydantic import EmailStr
-import pytest
 from twisted.logger import capturedLogs
 
 from canarytokens import queries
@@ -84,7 +83,6 @@ def test_log4shell_rendered_html(settings: SwitchboardSettings):
     assert "SRV01" in email_template
 
 
-@pytest.mark.skip()
 def test_sendgrid_send(
     settings: SwitchboardSettings, frontend_settings: FrontendSettings
 ):
