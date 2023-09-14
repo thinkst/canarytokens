1716687868e66c7381d577c1bc23fdb1873c3627
William Leighton Dawson <leighton@thinkst.com>
2023-09-14 23:46:20 +0200

    make black happy

--- canarytokens/glpat.py
+++ canarytokens/glpat.py
@@ -7,26 +7,29 @@ from canarytokens import tokens
 from canarytokens.models import GLPat
 from base64 import b64encode
 
+
 def get_glpat(
     token: tokens.Canarytoken,
     server: str,
     gitlab_broker_url: HttpUrl,
     broker_api_key: SecretStr,
     gl_token: Optional[str],
-    expires: Optional[str]
+    expires: Optional[str],
 ) -> GLPat:
     if gl_token and expires:
-        return GLPat(
-            {
-                "token": gl_token,
-                "expires": expires
-            }
-        )
+        return GLPat({"token": gl_token, "expires": expires})
 
     record = f"http://{server}/{token.value()}"
 
     target_url = f"{gitlab_broker_url}"
-    resp = requests.get(target_url, params={"url": b64encode(record.encode()).decode(), "ak": broker_api_key.get_secret_value()}, timeout=(5, 10))
+    resp = requests.get(
+        target_url,
+        params={
+            "url": b64encode(record.encode()).decode(),
+            "ak": broker_api_key.get_secret_value(),
+        },
+        timeout=(5, 10),
+    )
     resp.raise_for_status()
     resp_json = resp.json()
 
