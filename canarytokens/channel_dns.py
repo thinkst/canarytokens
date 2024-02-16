7b50b219051c0ae6f2afcdc8e6a2db5222dd2266
thinkst-az <156116192+thinkst-az@users.noreply.github.com>
2024-02-16 21:13:41 +0200

    Fix formatting (#348)

--- canarytokens/channel_dns.py
+++ canarytokens/channel_dns.py
@@ -118,7 +118,7 @@ class ChannelDNS(InputChannel):
             payload=dns.Record_A(ttl=10, address=self.frontend_settings.PUBLIC_IP),
             type=dns.A,
             auth=True,
-            ttl=300
+            ttl=300,
         )
         answers = [answer]
         authority: list[str] = []
@@ -143,7 +143,7 @@ class ChannelDNS(InputChannel):
             ),
             type=dns.SOA,
             auth=True,
-            ttl=300
+            ttl=300,
         )
         answers = [answer]
         authority = []
@@ -157,13 +157,15 @@ class ChannelDNS(InputChannel):
         """
         log.info(f"Building A record: ip = {self.frontend_settings.PUBLIC_IP}")
         ttl = 10
-        
+
         if name.lower().decode() in self.canary_domains:
             # This is a resolution of the apex domain, not a token, so we can bump up the TTL
-            ttl = 600 # 10 min seems plenty short enough to allow for IP changes without getting overloaded
+            ttl = 600  # 10 min seems plenty short enough to allow for IP changes without getting overloaded
 
         payload = dns.Record_A(ttl=ttl, address=self.frontend_settings.PUBLIC_IP)
-        answer = dns.RRHeader(name=name, payload=payload, type=dns.A, auth=True, ttl=ttl)
+        answer = dns.RRHeader(
+            name=name, payload=payload, type=dns.A, auth=True, ttl=ttl
+        )
         answers = [answer]
         authority: list[str] = []
         additional: list[str] = []
