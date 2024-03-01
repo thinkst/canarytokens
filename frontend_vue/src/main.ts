90de0a58e59682abb20bf59af08a6a4d68f4671e
Vittoria Toso <vittoria@thinkst.com>
2024-03-01 09:48:00 +0100

    Initial setup

---
+++ frontend_vue/src/main.ts
@@ -0,0 +1,12 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './style.css'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
