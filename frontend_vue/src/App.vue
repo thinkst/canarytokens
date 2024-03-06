7af155ab8fc99c16dadaf3bc980b4d0cb6bff019
Vittoria Toso <vittoria@thinkst.com>
2024-03-06 18:26:37 +0100

    export base components, add layout

--- frontend_vue/src/App.vue
+++ frontend_vue/src/App.vue
@@ -1,17 +1,27 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
   <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </header>
-  <RouterView />
+      <AppNavbar />
+  </header>
+  <main class="bg-grey-50 main-height">
+      <RouterView/>
+  </main>
+  <footer>
+      <AppFooter />
+  </footer>
 </template>
 
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+import AppNavbar from '@/components/AppNavbar.vue';
+import AppFooter from '@/components/AppFooter.vue';
+
+</script>
+
+
 <style scoped>
+
+.main-height {
+  min-height: calc(100svh - (100px + 100px));
+}
 
 </style>
---
+++ frontend_vue/src/components/base/_index.js
@@ -0,0 +1,26 @@
+// Globally register all base components for convenience, because they
+// will be used very frequently. Components are registered using the
+// PascalCased version of their file name.
+
+function registertBaseComponents(app) {
+    const componentFiles = import.meta.glob(
+        './_Base*.vue', { eager: true }
+    );
+
+
+    Object.entries(componentFiles).forEach(([path, m]) => {
+        // Get the component config
+        const componentConfig = m;
+        // Get the PascalCase version of the component name
+        const componentName = path
+            // Remove the "./_" from the beginning
+            .replace(/^\.\/_/, '')
+            // Remove the file extension from the end
+            .replace(/\.\w+$/, '');
+
+        // Globally register the component
+        app.component(componentName, componentConfig.default || componentConfig);
+    });
+}
+
+export default registertBaseComponents;
