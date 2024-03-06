3223ce9970a588c3e66f5d0f1e2fe915c7007749
Vittoria Toso <vittoria@thinkst.com>
2024-03-06 18:26:37 +0100

    export base components, add layout

---
+++ frontend_vue/src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text-grey-400 h-[100px] px-24 py-16 text-xs text-center grid grid-cols-1 lg:grid-cols-3 items-center">
+        <div class="flex justify-center py-16 lg:py-0 lg:justify-start">
+            <img src="@/assets/logo_footer.svg" alt="logo"/>
+        </div>
+        <div>
+            <p>Read Our <base-link href="#" target="_blank">Canarytokens Documentation</base-link></p>
+            <p>By Using This Service, You Agree to <base-link href="#" target="_blank">Our Terms of Use.</base-link></p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
--- frontend_vue/src/main.ts
+++ frontend_vue/src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+//@ts-ignore
+import registertBaseComponents from '@/components/base/_index.js';
 
 const app = createApp(App)
-
+registertBaseComponents(app);
 app.use(router)
 
 app.mount('#app')
