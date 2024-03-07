12970465bdfd7e0b33459bb89b0b5b275edd50c2
Vittoria Toso <vittoria@thinkst.com>
2024-03-07 12:15:34 +0100

    add menu animation, mobile menu, accessibility

--- frontend_vue/src/main.ts
+++ frontend_vue/src/main.ts
@@ -4,8 +4,15 @@ import router from './router'
 import './style.css'
 //@ts-ignore
 import registertBaseComponents from '@/components/base/_index.js';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+library.add(faBars, faXmark)
+
 registertBaseComponents(app);
 app.use(router)
 
