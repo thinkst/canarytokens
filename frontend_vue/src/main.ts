b6b4386508170b392529f0de2701821d3890d04e
Vittoria Toso <vittoria@thinkst.com>
2024-03-07 13:33:15 +0100

    add layout main + icons

--- frontend_vue/src/main.ts
+++ frontend_vue/src/main.ts
@@ -6,12 +6,12 @@ import './style.css'
 import registertBaseComponents from '@/components/base/_index.js';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faXmark, faLink } from '@fortawesome/free-solid-svg-icons';
 
 const app = createApp(App)
 
 app.component('font-awesome-icon', FontAwesomeIcon)
-library.add(faBars, faXmark)
+library.add(faBars, faXmark, faLink)
 
 registertBaseComponents(app);
 app.use(router)
