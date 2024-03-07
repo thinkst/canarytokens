3424fbfbe0af7d6b9bb6eaa282539d386c3b8833
Vittoria Toso <vittoria@thinkst.com>
2024-03-07 13:33:15 +0100

    add layout main + icons

---
+++ frontend_vue/src/layout/LayoutMain.vue
@@ -0,0 +1,17 @@
+<template>
+    <main class="flex items-center justify-center main-height bg-grey-50">
+        <div class="flex-grow m-24 md:mx-[10svw] bg-white rounded-lg shadow-lg shadow-grey-200/40 min-h-[50vh] p-24">
+            <slot ></slot>
+        </div>
+    </main>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+.main-height {
+  min-height: calc(100svh - (100px + 100px));
+}
+</style>
