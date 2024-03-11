9fbf3502a6b82f2d54f071c591e0a38c46630669
Vittoria Toso <vittoria@thinkst.com>
2024-03-11 11:56:35 +0100

    update routes and layout

--- frontend_vue/src/views/HomeView.vue
+++ frontend_vue/src/views/HomeView.vue
@@ -1,8 +1,9 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <main>
+  <AppLayoutGrid>
     <h1>Home Page Content</h1>
-  </main>
+  </AppLayoutGrid>
 </template>
+
+<script setup lang="ts">
+import AppLayoutGrid from '@/layout/AppLayoutGrid.vue';
+</script>
