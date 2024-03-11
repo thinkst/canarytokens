e0cf81d81047512083704c2882c08e285b2185ee
Vittoria Toso <vittoria@thinkst.com>
2024-03-11 13:21:12 +0100

    add Grid layout

--- frontend_vue/src/components/AppLogo.vue
+++ frontend_vue/src/components/AppLogo.vue
@@ -1,6 +1,6 @@
 <template>
     <a>
-        <img src="../assets/logo.png" alt="Canary Tokens" class="w-[12rem] min-w-[12rem]"/>
+        <img src="../assets/logo.png" alt="Canary Tokens" class="w-[12rem] min-w-[12rem]" />
     </a>
 </template>
 
--- frontend_vue/src/layout/AppLayoutGrid.vue
+++ frontend_vue/src/layout/AppLayoutGrid.vue
@@ -1,7 +1,14 @@
 <template>
-    <div class="grid">
-    <slot></slot>
+    <div class="grid layout-grid">
+        <slot></slot>
     </div>
 </template>
 
 <script setup lang="ts"></script>
+
+<style scoped>
+.layout-grid {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    gap: 1rem;
+}
+</style>
