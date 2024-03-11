e4c32eb9ad6df00b39ea562c9efe778668909b20
Vittoria Toso <vittoria@thinkst.com>
2024-03-11 11:56:35 +0100

    update routes and layout

--- frontend_vue/src/components/AppLogo.vue
+++ frontend_vue/src/components/AppLogo.vue
@@ -1,5 +1,5 @@
 <template>
-    <a >
+    <a>
         <img src="../assets/logo.png" alt="Canary Tokens" class="w-[12rem] min-w-[12rem]"/>
     </a>
 </template>
---
+++ frontend_vue/src/layout/AppLayoutGrid.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="grid">
+    <slot></slot>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
