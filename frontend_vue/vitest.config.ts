c3dab593e2d515b1eea780dae1813be55833bd5c
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-03-13 12:15:14 +0100

    Add Base Button component (#356)
    * Add button component
    
    * Add button tests

---
+++ frontend_vue/vitest.config.ts
@@ -0,0 +1,12 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vitest/config';
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+    plugins: [vue()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+    },
+});
