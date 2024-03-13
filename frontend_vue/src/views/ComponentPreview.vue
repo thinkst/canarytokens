34f4a5e8d826e987cbb6d84063bdd9ab39858eb3
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-03-13 12:15:14 +0100

    Add Base Button component (#356)
    * Add button component
    
    * Add button tests

---
+++ frontend_vue/src/views/ComponentPreview.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-col w-[200px] gap-16">
+        <h1>Buttons</h1>
+        <base-button>Primary</base-button>
+        <base-button variant="secondary">Secondary</base-button>
+        <base-button variant="text">Text!!</base-button>
+    </div>
+</template>
+
+<script setup lang="ts">
+// TODO: remove this view - for internal use only
+</script>
