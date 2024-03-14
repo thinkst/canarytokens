a7c2d0582fbdbf7eb1bfccdaa6333d10a28523d7
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-03-14 10:32:50 +0100

    T7755 add modal custom (#357)
    * add and style Base Modal
    
    * Add Base Modal + Add Token + How to use + confirm new token
    
    * add Back Button events
    
    * add/review Buttons + add Modal to Component view
    
    * update buttons and component preview
    
    * update classes and footer slot

--- frontend_vue/src/main.ts
+++ frontend_vue/src/main.ts
@@ -6,12 +6,18 @@ import './style.css'
 import registertBaseComponents from '@/components/base/_index.js';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faBars, faXmark, faLink } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faXmark, faLink, faQuestion, faCircleQuestion, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { createVfm } from 'vue-final-modal'
+import 'vue-final-modal/style.css'
+
 
 const app = createApp(App)
 
 app.component('font-awesome-icon', FontAwesomeIcon)
-library.add(faBars, faXmark, faLink)
+library.add(faBars, faXmark, faLink, faQuestion, faCircleQuestion, faAngleLeft)
+
+const vfm = createVfm()
+app.use(vfm)
 
 registertBaseComponents(app);
 app.use(router)
