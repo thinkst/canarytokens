6d7ca1b934bb4e61b62a450a07252f986cd1f610
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-03-14 12:13:25 +0100

    Optimize tailwind classes refine modal (#358)
    * fix classes button + add modal animations
    
    * fix classes button + add modal animations

--- frontend_vue/src/components/base/_BaseButton.vue
+++ frontend_vue/src/components/base/_BaseButton.vue
@@ -26,14 +26,28 @@ const props = defineProps({
 const buttonClass = computed(() => {
     switch (props.variant) {
         case 'primary':
-            return 'relative bg-green hover:bg-green-300 disabled:pointer-events-none disabled:bg-grey-200 disabled:shadow-solid-shadow-grey disabled:border-grey-300 disabled:text-grey-400 active:shadow-none active:top-[0.15rem] focus-visible:outline-0 focus-visible:border-green-800  focus:bg-green-300 focus:border-green-800 focus:outline-0 text-white border shadow-solid-shadow-green border-green-800 font-semibold rounded-full lg:px-32 lg:py-16 px-16 py-8';
+            return 'primary'
         case 'secondary':
-            return 'relative bg-transparent hover:bg-green-50 hover:text-green-500 focus:text-green-500 disabled:pointer-events-none disabled:bg-grey-200 disabled:shadow-solid-shadow-grey disabled:border-grey-300 disabled:text-grey-400 active:shadow-none active:top-[0.15rem] focus-visible:outline-0 focus:bg-green-100 focus:border-green-200 focus:outline-0 text-green-600 border shadow-solid-shadow-green-300 border-green-200 font-semibold rounded-full lg:px-32 lg:py-16 px-16 py-8';
+            return 'secondary'
         case 'text':
-            return 'font-semibold hover:text-green-500 focus:text-green-800 text-grey-500 rounded-full px-16 py-8'
+            return 'text'
         default:
-            return 'relative bg-green hover:bg-green-300 disabled:pointer-events-none disabled:bg-grey-200 disabled:shadow-solid-shadow-grey disabled:border-grey-300 disabled:text-grey-400 active:shadow-none active:top-[0.25rem] focus-visible:outline-0 focus-visible:border-green-800  focus:bg-green-300 focus:border-green-800 focus:outline-0 text-white border shadow-solid-shadow-green border-green-800 font-semibold rounded-full lg:px-32 lg:py-16 px-16 py-8';
+            return 'primary'
     }
 })
 
 </script>
+
+<style scoped>
+.primary {
+    @apply relative bg-green hover:bg-green-300 disabled:pointer-events-none disabled:bg-grey-200 disabled:shadow-solid-shadow-grey disabled:border-grey-300 disabled:text-grey-400 active:shadow-none active:top-[0.15rem] focus-visible:outline-0 focus-visible:border-green-800 focus:bg-green-300 focus:border-green-800 focus:outline-0 text-white border shadow-solid-shadow-green border-green-800 font-semibold rounded-full px-16 py-8
+}
+
+.secondary {
+    @apply relative bg-white hover:bg-green-50 hover:text-green-500 focus:text-green-500 disabled:pointer-events-none disabled:bg-grey-200 disabled:shadow-solid-shadow-grey disabled:border-grey-300 disabled:text-grey-400 active:shadow-none active:top-[0.15rem] focus-visible:outline-0 focus:bg-green-100 focus:border-green-200 focus:outline-0 text-green-600 border shadow-solid-shadow-green-300 border-green-200 font-semibold rounded-full px-16 py-8
+}
+
+.text {
+    @apply relative font-semibold hover:text-green-500 focus:text-green-800 text-grey-500 rounded-full px-16 py-8
+}
+</style>
