2b2370236ddaa8c60f17e499edbdc37335553d73
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-03-14 10:32:50 +0100

    T7755 add modal custom (#357)
    * add and style Base Modal
    
    * Add Base Modal + Add Token + How to use + confirm new token
    
    * add Back Button events
    
    * add/review Buttons + add Modal to Component view
    
    * update buttons and component preview
    
    * update classes and footer slot

--- frontend_vue/src/views/ComponentPreview.vue
+++ frontend_vue/src/views/ComponentPreview.vue
@@ -1,12 +1,23 @@
 <template>
-    <div class="flex flex-col w-[200px] gap-16">
-        <h1>Buttons</h1>
-        <base-button>Primary</base-button>
-        <base-button variant="secondary">Secondary</base-button>
-        <base-button variant="text">Text!!</base-button>
-    </div>
+  <div class="flex flex-col w-[200px] gap-16">
+    <h1>Buttons</h1>
+    <base-button>Primary</base-button>
+    <base-button variant="secondary">Secondary</base-button>
+    <base-button variant="text">Text!!</base-button>
+  </div>
+  <div class="flex flex-col w-[200px] gap-16">
+    <h1>Modal</h1>
+    <BaseButton @click="() => open()">Check modal</BaseButton>
+  </div>
 </template>
 
 <script setup lang="ts">
 // TODO: remove this view - for internal use only
+import { useModal } from 'vue-final-modal'
+import ModalAddToken from '@/components/ModalAddToken.vue';
+
+  const { open } = useModal({
+    component: ModalAddToken,
+  })
+
 </script>
