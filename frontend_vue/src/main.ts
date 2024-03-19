672c6ceb35a7690f662766d51902654a6a5a941a
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-03-19 09:46:18 +0100

    T7744 add token card component (#359)
    * add token card + fix formatter
    
    * restructure button semantic
    
    * Fix typescript for global components
    
    * add a11y check, add modal sample, add test
    
    * add editorconfig copy
    
    * fix after review: add base button css, remove console log

--- frontend_vue/src/main.ts
+++ frontend_vue/src/main.ts
@@ -3,23 +3,24 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 //@ts-ignore
-import registertBaseComponents from '@/components/base/_index.js';
+import registertBaseComponents from '@/components/base/_index.ts';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faBars, faXmark, faLink, faQuestion, faCircleQuestion, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faXmark, faLink, faQuestion, faCircleQuestion, faAngleLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { createVfm } from 'vue-final-modal'
 import 'vue-final-modal/style.css'
 
 
 const app = createApp(App)
 
-app.component('font-awesome-icon', FontAwesomeIcon)
-library.add(faBars, faXmark, faLink, faQuestion, faCircleQuestion, faAngleLeft)
+app.component('FontAwesomeIcon', FontAwesomeIcon)
+library.add(faBars, faXmark, faLink, faQuestion, faCircleQuestion, faAngleLeft, faArrowRight)
 
 const vfm = createVfm()
 app.use(vfm)
 
 registertBaseComponents(app);
+
 app.use(router)
 
 app.mount('#app')
