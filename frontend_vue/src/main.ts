f2ed167fd037d2d59838e813d0dc333a8065fef4
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-03-21 13:08:42 +0100

    Add copy button, test, package coverage for testing (#362)

--- frontend_vue/src/main.ts
+++ frontend_vue/src/main.ts
@@ -1,25 +1,30 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './style.css'
 //@ts-ignore
 import registertBaseComponents from '@/components/base/_index.ts';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faBars, faXmark, faLink, faQuestion, faCircleQuestion, faAngleLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faXmark, faLink, faQuestion, faCircleQuestion, faAngleLeft, faArrowRight, faCheck, faCopy } from '@fortawesome/free-solid-svg-icons';
 import { createVfm } from 'vue-final-modal'
+import {
+  vTooltip,
+} from 'floating-vue'
 import 'vue-final-modal/style.css'
-
+import 'floating-vue/dist/style.css'
+import './style.css'
 
 const app = createApp(App)
 
 app.component('FontAwesomeIcon', FontAwesomeIcon)
-library.add(faBars, faXmark, faLink, faQuestion, faCircleQuestion, faAngleLeft, faArrowRight)
+library.add(faBars, faXmark, faLink, faQuestion, faCircleQuestion, faAngleLeft, faArrowRight, faCheck, faCopy)
 
 const vfm = createVfm()
 app.use(vfm)
 
 registertBaseComponents(app);
+
+app.directive('tooltip', vTooltip)
 
 app.use(router)
 
