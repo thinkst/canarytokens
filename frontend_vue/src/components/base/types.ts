e7d82607fd1760c1c7aa8a3d493846b4014a45c8
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-03-25 08:54:18 +0100

    T7742 alert component (#363)
    * add Notification box component + global export Base components
    
    * remove types and restore old setting
    
    * fix responsive, fix types
    
    * add tests
    
    * rename component
    
    * aria hidden icons
    
    * add enum to base button and message box
    
    * fix types for base state and color avaints

---
+++ frontend_vue/src/components/base/types.ts
@@ -0,0 +1,17 @@
+export interface BaseStateVariantType {
+  danger: string;
+  warning: string;
+  info: string;
+}
+
+export interface BaseColorVariantType extends BaseStateVariantType {
+  primary: string;
+  secondary: string;
+}
+
+export interface BaseButtonVariantType extends BaseColorVariantType {
+  text: string;
+}
+
+export type ButtonVariantType = keyof BaseButtonVariantType;
+export type NotificationBoxVariantType = keyof BaseStateVariantType;
