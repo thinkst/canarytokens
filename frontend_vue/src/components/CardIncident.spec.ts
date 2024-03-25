a858dac298b857492314b8ecfc8e688772d7f6cd
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-03-25 16:37:10 +0100

    T7742 alert component (#365)
    * add Notification box component + global export Base components
    
    * remove types and restore old setting
    
    * fix responsive, fix types
    
    * add tests
    
    * rename component
    
    * rebase
    
    * remove consolelog

---
+++ frontend_vue/src/components/CardIncident.spec.ts
@@ -0,0 +1,36 @@
+import { mount } from '@vue/test-utils';
+import CardIncident from './CardIncident.vue';
+
+describe('CardIncident.vue', () => {
+  test('is called', () => {
+    const wrapper = mount(CardIncident);
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it('renders the incidentPreviewInfo prop correctly', async () => {
+    const incidentPreviewInfo = {
+      Date: '2024-03-22 16:01:08.976860',
+      IP: '84.138.198.174',
+      Channel: 'HTTP',
+    };
+    const incidentId = '123';
+
+    const wrapper = mount(CardIncident, {
+      props: { incidentPreviewInfo, incidentId },
+    });
+
+    expect(wrapper.text()).toContain(incidentPreviewInfo.Date);
+    expect(wrapper.text()).toContain(incidentPreviewInfo.IP);
+    expect(wrapper.text()).toContain(incidentPreviewInfo.Channel);
+  });
+
+  it('emits a "click" event when clicked', async () => {
+    const wrapper = mount(CardIncident);
+
+    await wrapper.find('button').trigger('click');
+
+    expect(wrapper.emitted()).toHaveProperty('click');
+
+    expect(wrapper.emitted('click')).toHaveLength(1);
+  });
+});
