56b1f6ed9d453cdd60af6331617b11136590be4d
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-03-25 08:54:18 +0100

    T7742 alert component (#363)
    * add Notification box component + global export Base components
    
    * remove types and restore old setting
    
    * fix responsive, fix types
    
    * add tests
    
    * rename component
    
    * aria hidden icons
    
    * add enum to base button and message box
    
    * fix types for base state and color avaints

--- frontend_vue/vite.config.ts
+++ frontend_vue/vite.config.ts
@@ -1,16 +1,35 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      dirs: ['src/components/base'],
+      resolvers: [
+        //@ts-ignore
+        (name) => {
+          if (name.startsWith('Base')) {
+            return {
+              name: name,
+              path: `src/components/base/${name}.vue`,
+            };
+          }
+        },
+      ],
+      // valid file extensions for components.
+      extensions: ['vue'],
+      dts: true,
+      // search for subdirectories
+      deep: false,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
