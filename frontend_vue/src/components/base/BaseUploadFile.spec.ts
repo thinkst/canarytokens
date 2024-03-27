cb69819ecdf492b1a3877f58ffda365527ab5358
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-03-27 14:45:52 +0100

    T7739 upload file component (#368)
    * add upload file component
    
    * add test and minor TS fixes
    
    * remove failing test
    
    * clean convert-bytes util
    
    * polish preview

---
+++ frontend_vue/src/components/base/BaseUploadFile.spec.ts
@@ -0,0 +1,75 @@
+import { mount } from '@vue/test-utils';
+import { describe, it, expect, afterEach, vi } from 'vitest';
+
+import BaseUploadFile from '@/components/base/BaseUploadFile.vue';
+import BaseButton from './BaseButton.vue';
+
+describe('BaseUploadFile', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('emits file-selected event when a valid file is selected', async () => {
+    const wrapper = mount(BaseUploadFile, {
+      props: {
+        allowedFiles: 'image/jpeg',
+        infoAllowedFile: 'JPEG max size 1MB',
+        maxSize: 1024 * 1024,
+      },
+      global: {
+        stubs: { BaseButton },
+      },
+    });
+
+    const file = new File(['file contents'], 'test.jpg', {
+      type: 'image/jpeg',
+      size: 500000,
+    } as File);
+
+    await (wrapper.vm as any).fileValidation(file);
+    expect(wrapper.emitted('file-selected')).toBeTruthy();
+  });
+
+  it('emits file-upload-error event when an invalid file is selected', async () => {
+    const wrapper = mount(BaseUploadFile, {
+      props: {
+        allowedFiles: 'image/jpeg',
+        maxSize: 1024 * 1024,
+        infoAllowedFile: 'JPEG max size 1MB',
+      },
+      global: {
+        stubs: { BaseButton },
+      },
+    });
+
+    const file = new File(['file contents'], 'test.png', {
+      type: 'image/png',
+      size: 1024 * 1025,
+    } as File);
+
+    await (wrapper.vm as any).fileValidation(file);
+
+    expect(wrapper.emitted('file-upload-error')).toBeTruthy();
+  });
+
+  it('displays error message when an invalid file format is selected', async () => {
+    const wrapper = mount(BaseUploadFile, {
+      props: {
+        allowedFiles: 'image/png',
+        maxSize: 1024 * 1024,
+        infoAllowedFile: 'PNG max size 1MB',
+      },
+      global: {
+        stubs: { BaseButton },
+      },
+    });
+
+    const file = new File(['file contents'], 'test.zip', {
+      type: 'application/zip',
+      size: 1024 * 1025,
+    } as File);
+
+    await (wrapper.vm as any).fileValidation(file);
+    expect(wrapper.html()).toContain('application/zip files are not allowed');
+  });
+});
