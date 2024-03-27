240f08aaee842ef1fc590c6230f62ff3fa9ecb2c
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-03-27 16:09:57 +0100

    run formatting, add local vars (#370)

--- frontend_vue/src/components/AppFooter.vue
+++ frontend_vue/src/components/AppFooter.vue
@@ -1,16 +1,31 @@
 <template>
-    <div class="text-grey-400 h-[100px] px-24 py-16 text-xs text-center grid grid-cols-1 lg:grid-cols-3 items-center">
-        <div class="flex justify-center py-16 lg:py-0 lg:justify-start">
-            <a href="https://canary.tools/" target="_blank"><img src="@/assets/logo_footer.svg" alt="logo" /></a>
-        </div>
-        <div>
-            <p>Read Our <base-link href="https://docs.canarytokens.org/guide/" target="_blank">Canarytokens
-                    Documentation</base-link></p>
-            <p>By Using This Service, You Agree to <base-link href="/legal">Our Terms of Use.</base-link></p>
-        </div>
+  <div
+    class="text-grey-400 h-[100px] px-24 py-16 text-xs text-center grid grid-cols-1 lg:grid-cols-3 items-center"
+  >
+    <div class="flex justify-center py-16 lg:py-0 lg:justify-start">
+      <a
+        href="https://canary.tools/"
+        target="_blank"
+        ><img
+          src="@/assets/logo_footer.svg"
+          alt="logo"
+      /></a>
     </div>
+    <div>
+      <p>
+        Read Our
+        <base-link
+          href="https://docs.canarytokens.org/guide/"
+          target="_blank"
+          >Canarytokens Documentation</base-link
+        >
+      </p>
+      <p>
+        By Using This Service, You Agree to
+        <base-link href="/legal">Our Terms of Use.</base-link>
+      </p>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
