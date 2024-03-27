8fdaac1345952cc7a11ef32094993cc90b3c1251
Vittoria Toso <vittoria@thinkst.com>
2024-03-27 09:39:48 +0100

    clean convert-bytes util

--- frontend_vue/src/components/base/utils.ts
+++ frontend_vue/src/components/base/utils.ts
@@ -1,18 +1,10 @@
 export function convertBytes(
-  // copied from https://gist.github.com/lanqy/5193417?permalink_comment_id=4755323#gistcomment-4755323
-  bytes: number,
-  options: { useBinaryUnits?: boolean; decimals?: number } = {}
+  // gracefully copied from https://gist.github.com/lanqy/5193417?permalink_comment_id=4755323#gistcomment-4755323
+  bytes: number
 ): string {
-  const { useBinaryUnits = false, decimals = 2 } = options;
-
-  if (decimals < 0) {
-    throw new Error(`Invalid decimals ${decimals}`);
-  }
-
-  const base = useBinaryUnits ? 1024 : 1000;
-  const units = useBinaryUnits
-    ? ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
-    : ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const decimals = 2;
+  const base = 1000;
+  const units = ['Bytes', 'KB', 'MB', 'GB'];
 
   const i = Math.floor(Math.log(bytes) / Math.log(base));
 
