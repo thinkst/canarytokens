87156831163e059481506b1753484d4a550b4475
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-03-27 16:09:57 +0100

    run formatting, add local vars (#370)

--- frontend_vue/src/components/AppNavbarMenuMobile.vue
+++ frontend_vue/src/components/AppNavbarMenuMobile.vue
@@ -1,96 +1,120 @@
 <template>
-    <button :aria-expanded="showMobileMenu" aria-controls="mobile_menu"
-        class="bg-grey-100 md:hidden w-[36px] h-[36px] rounded-full self-center mr-16 overflow-hidden hover:bg-grey-200 active:bg-green focus:bg-grey-200"
-        @click="handleShowMobileMenu">
-        <Transition name="icon">
-            <span v-if="showMobileMenu" class="duration-150 ease-in-out transition-margin"><font-awesome-icon
-                    class="text-grey-500" icon="xmark" /></span>
-            <span v-else class="duration-150 ease-in-out transition-margin"><font-awesome-icon class="text-grey-500"
-                    icon="bars" /></span>
-        </Transition>
-    </button>
-    <Transition>
-        <nav role="navigation" id="mobile_menu"
-            class="absolute transition-left ease-in-out duration-300 w-full text-center md:hidden bg-green top-[80px] py-32 min-h-svh z-50 motion-reduce:transition-none motion-reduce:hover:transform-none"
-            v-if="showMobileMenu">
-            <ul class="uppercase">
-                <li v-for="item in menuItems" :key="item.name" class="py-8">
-                    <RouterLink :to="item.path" class="text-green-200 hover:text-white mobile-link"
-                        @click="handleShowMobileMenu">
-                        {{ item.name }}
-                    </RouterLink>
-                </li>
-                <li class="py-8 text-green-200 cursor-pointer hover:text-white mobile-link">
-                    <a href="#">
-                        <font-awesome-icon icon="link" class="w-[0.8rem] pr-8" />Documentation
-                    </a>
-                </li>
-            </ul>
-        </nav>
+  <button
+    :aria-expanded="showMobileMenu"
+    aria-controls="mobile_menu"
+    class="bg-grey-100 md:hidden w-[36px] h-[36px] rounded-full self-center mr-16 overflow-hidden hover:bg-grey-200 active:bg-green focus:bg-grey-200"
+    @click="handleShowMobileMenu"
+  >
+    <Transition name="icon">
+      <span
+        v-if="showMobileMenu"
+        class="duration-150 ease-in-out transition-margin"
+        ><font-awesome-icon
+          class="text-grey-500"
+          icon="xmark"
+      /></span>
+      <span
+        v-else
+        class="duration-150 ease-in-out transition-margin"
+        ><font-awesome-icon
+          class="text-grey-500"
+          icon="bars"
+      /></span>
     </Transition>
+  </button>
+  <Transition>
+    <nav
+      role="navigation"
+      id="mobile_menu"
+      class="absolute transition-left ease-in-out duration-300 w-full text-center md:hidden bg-green top-[80px] py-32 min-h-svh z-50 motion-reduce:transition-none motion-reduce:hover:transform-none"
+      v-if="showMobileMenu"
+    >
+      <ul class="uppercase">
+        <li
+          v-for="item in menuItems"
+          :key="item.name"
+          class="py-8"
+        >
+          <RouterLink
+            :to="item.path"
+            class="text-green-200 hover:text-white mobile-link"
+            @click="handleShowMobileMenu"
+          >
+            {{ item.name }}
+          </RouterLink>
+        </li>
+        <li
+          class="py-8 text-green-200 cursor-pointer hover:text-white mobile-link"
+        >
+          <a href="#">
+            <font-awesome-icon
+              icon="link"
+              class="w-[0.8rem] pr-8"
+            />Documentation
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </Transition>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { RouterLink } from 'vue-router'
+import { RouterLink } from 'vue-router';
 import type { PropType } from 'vue';
 
 type menuItemsType = {
-    name: string,
-    path: string
-}
+  name: string;
+  path: string;
+};
 
 defineProps({
-    menuItems: {
-        type: Array as PropType<menuItemsType[]>,
-        required: true
-    },
-})
+  menuItems: {
+    type: Array as PropType<menuItemsType[]>,
+    required: true,
+  },
+});
 
-const showMobileMenu = ref(false)
+const showMobileMenu = ref(false);
 
 function handleShowMobileMenu() {
-    showMobileMenu.value = !showMobileMenu.value;
+  showMobileMenu.value = !showMobileMenu.value;
 }
-
-
 </script>
 
 <style scoped lang="scss">
 .mobile-link {
-    &.router-link-active {
-        color: hsl(0, 0%, 100%);
-        position: relative;
-    }
-
+  &.router-link-active {
+    color: hsl(0, 0%, 100%);
+    position: relative;
+  }
 }
 
 .v-enter-to,
 .v-leave-from {
-    right: 0;
+  right: 0;
 }
 
 .v-enter-from,
 .v-leave-to {
-    right: -765px;
+  right: -765px;
 }
-
 
 .icon-enter-to,
 .icon-leave-from {
-    margin-right: 0;
+  margin-right: 0;
 }
 
 .icon-enter-from {
-    margin-right: -15px;
+  margin-right: -15px;
 }
 
 .icon-leave-to {
-    margin-left: -15px;
+  margin-left: -15px;
 }
 
 .icon-enter-active,
 .icon-leave-active {
-    opacity: 0;
+  opacity: 0;
 }
 </style>
--- frontend_vue/src/components/ModalContentNewToken.vue
+++ frontend_vue/src/components/ModalContentNewToken.vue
@@ -1,17 +1,25 @@
 <template>
-    <img src="../assets/token_icons/s3_bucket_active.png" alt="Token logo" class="w-[8rem]" />
-    <h2 class="text-xl font-semibold text-center">Your S3 bucket Token is active!</h2>
-    <p class="text-sm text-center">Copy this URL to your clipboard and use as you wish</p>
+  <img
+    src="../assets/token_icons/s3_bucket_active.png"
+    alt="Token logo"
+    class="w-[8rem]"
+  />
+  <h2 class="text-xl font-semibold text-center">
+    Your S3 bucket Token is active!
+  </h2>
+  <p class="text-sm text-center">
+    Copy this URL to your clipboard and use as you wish
+  </p>
 
-    <div class="copy-token">
-        <!-- add here token url, code and whatever else -->
-    </div>
+  <div class="copy-token">
+    <!-- add here token url, code and whatever else -->
+  </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue';
 
 onMounted(() => {
-    console.log('Token is active')
-})
+  console.log('Token is active');
+});
 </script>
