4bda83ddcb680f85e9c2d3d9be2f0d789bbe41b1
Vittoria Toso <vittoria@thinkst.com>
2024-04-08 15:29:27 +0200

    fix vite local API

--- frontend_vue/vite.config.ts
+++ frontend_vue/vite.config.ts
@@ -2,52 +2,61 @@ import { fileURLToPath, URL } from 'node:url';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { loadEnv } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      dirs: ['src/components/base'],
-      resolvers: [
-        //@ts-ignore
-        (name) => {
-          if (name.startsWith('Base')) {
-            return {
-              name: name,
-              path: `src/components/base/${name}.vue`,
-            };
-          }
-        },
-      ],
-      // valid file extensions for components.
-      extensions: ['vue'],
-      dts: true,
-      // search for subdirectories
-      deep: false,
-    }),
-  ],
-  // fix import error
-  // https://github.com/fawmi/vue-google-maps/issues/148#issuecomment-1235143844
-  optimizeDeps: {
-    include: ['@fawmi/vue-google-maps', 'fast-deep-equal'],
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const processEnvValues = {
+    'process.env': Object.entries(env).reduce((prev, [key, val]) => {
+      return {
+        ...prev,
+        [key]: val,
+      };
+    }, {}),
+  };
+
+  return {
+    plugins: [
+      vue(),
+      Components({
+        dirs: ['src/components/base'],
+        resolvers: [
+          //@ts-ignore
+          (name) => {
+            if (name.startsWith('Base')) {
+              return {
+                name: name,
+                path: `src/components/base/${name}.vue`,
+              };
+            }
+          },
+        ],
+        // valid file extensions for components.
+        extensions: ['vue'],
+        dts: true,
+        // search for subdirectories
+        deep: false,
+      }),
+    ],
+    optimizeDeps: {
+      include: ['@fawmi/vue-google-maps', 'fast-deep-equal'],
     },
-  },
-  define: {
-    'import.meta.env': JSON.stringify(process.env),
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    proxy: {
-      '^/api/*': {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+    define: processEnvValues,
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      proxy: {
+        '^/api/*': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
