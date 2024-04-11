1b4c1a7cd473b3c7325e95a1962933ae0a7b2e68
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-11 17:10:13 +0200

    Add map component + History Page (#376)
    * Add map component + Histry Page
    
    * fix zoom and cluster map
    
    * fix typo
    
    * add helper

---
+++ frontend_vue/src/views/HistoryView.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2
+    v-if="selectedTokenType && selectedTokenRef"
+    class="mb-24 text-xl text-center text-grey-800"
+  >
+    {{ selectedTokenType }}: {{ selectedTokenRef }}
+  </h2>
+  <AppLayoutTwoColumns>
+    <HistoryToken
+      @update-token-title="(type, token) => handleTokenTitle(type, token)"
+    />
+  </AppLayoutTwoColumns>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import AppLayoutTwoColumns from '@/layout/AppLayoutTwoColumns.vue';
+import HistoryToken from '@/components/HistoryToken.vue';
+import { tokenServices } from '@/utils/tokenServices';
+
+const selectedTokenType = ref('');
+const selectedTokenRef = ref('');
+
+function handleTokenTitle(type: string, token: string) {
+  selectedTokenType.value = tokenServices[type].label;
+  selectedTokenRef.value = token;
+}
+</script>
