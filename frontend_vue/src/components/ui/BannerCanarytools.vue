724b38d96e5740bac3bfe5f8c5b8cd9131b3aeec
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-12 17:09:17 +0200

    History page - add Incident Details and fix layout (#380)
    * first iteration
    
    * add incident details
    
    * fix typescript, fix naming highlightBoolean, fix example
    
    * fix type
    
    * Remove incident details detail

---
+++ frontend_vue/src/components/ui/BannerCanarytools.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="p-16 mt-16 text-center bg-grey-100 h-[15svh]">
+    Banner Placeholder
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
