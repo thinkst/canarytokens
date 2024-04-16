afcb6e9b98ad7675a2048f4e16760b95be7a5d4a
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-16 17:13:38 +0200

    Generate aws keys token (#387)
    * add aws key token
    
    * update validation field
    
    * update display snippet

---
+++ frontend_vue/src/components/tokens/aws_keys/TokenDisplay.vue
@@ -0,0 +1,30 @@
+<template>
+  <base-code-snippet
+    lang="javascript"
+    label="AWS token"
+    :code="AWSKeyCode"
+    multiline
+    custom-height="10rem"
+  ></base-code-snippet>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+type AWSKeysDataType = {
+  aws_access_key_id: string;
+  aws_secret_access_key: string;
+  output: string;
+  region: string;
+};
+
+const props = defineProps<{
+  tokenData: AWSKeysDataType;
+}>();
+
+const AWSKeyCode = ref(`[default]
+aws_access_key_id = ${props.tokenData.aws_access_key_id}
+aws_secret_access_key = ${props.tokenData.aws_secret_access_key}
+output = ${props.tokenData.output}
+region = ${props.tokenData.region}`);
+</script>
---
+++ frontend_vue/src/components/tokens/aws_keys/howToUse.ts
@@ -0,0 +1,4 @@
+export const howToUse = [
+  'These credentials are often stored in a file called ~/.aws/credentials on linux/OSX systems. Generate a fake credential pair for your senior developers and sysadmins and keep it on their machines. If someone tries to access AWS with the pair you generated for Bob, chances are that Bob`s been compromised.',
+  'Place the credentials in private code repositories. If the token is triggered, it means that someone is accessing that repo without permission',
+];
