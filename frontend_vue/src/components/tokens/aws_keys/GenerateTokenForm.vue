c2dd3b9c63d1b6f9852f764519adc9de8bc69d38
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-16 17:13:38 +0200

    Generate aws keys token (#387)
    * add aws key token
    
    * update validation field
    
    * update display snippet

---
+++ frontend_vue/src/components/tokens/aws_keys/GenerateTokenForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <BaseFormTextField
+    id="email"
+    type="text"
+    placeholder="Email"
+    label="Email"
+    full-width
+    helper-message="Provide an email address or webhook URL"
+  />
+  <BaseFormTextField
+    id="webhook_url"
+    type="text"
+    placeholder="URL"
+    label="URL"
+    full-width
+    helper-message="Provide an email address or webhook URL"
+  />
+  <BaseFormTextField
+    id="memo"
+    label="Add Note"
+    multiline
+    required
+    full-width
+    helper-message="Reminder note when this token is triggered. For instance: AWS keys placed on Jim's laptop"
+  ></BaseFormTextField>
+</template>
+
+<script setup lang="ts"></script>
