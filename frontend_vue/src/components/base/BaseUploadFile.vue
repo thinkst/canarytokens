33e8bb4630061a758895537a3749dc560a57b60d
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-17 19:06:20 +0200

    Generate custom image web bug (#390)
    * first iteration
    
    * fix validation, tests
    
    * update validators add manage token
    
    * removed unused component declaration:
    
    * hide wise man
    
    * remove validation, move input under note

--- frontend_vue/src/components/base/BaseUploadFile.vue
+++ frontend_vue/src/components/base/BaseUploadFile.vue
@@ -47,7 +47,7 @@
         </p>
 
         <BaseButton
-          class=""
+          class="upload-button"
           :disabled="disabled"
           :variant="!showFileName ? 'secondary' : 'text'"
           @click.stop="
@@ -57,14 +57,6 @@
         >
       </div>
       <p
-        v-if="isNotValidFile"
-        id="error-message"
-        class="pt-16 text-sm font-semibold leading-4 text-red"
-      >
-        {{ isNotValidFileMessage }}
-      </p>
-      <p
-        v-if="hasError"
         id="error-message"
         class="pt-16 text-sm font-semibold leading-4 text-red"
       >
@@ -72,125 +64,102 @@
       </p>
       <input
         v-bind="$attrs"
-        id="dropzone-file"
+        :id="id"
         ref="fileInput"
         type="file"
-        :accept="allowedFiles"
         class="hidden"
         :disabled="disabled"
-        :max-size="maxSize"
         @change.stop="handleFileChange"
+        @blur="handleBlur"
       />
     </label>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, computed, onMounted } from 'vue';
-import { convertBytes } from './utils.ts';
+import { ref, computed, onMounted, toRef } from 'vue';
 import FolderIcon from '@/components/icons/FolderIcon.vue';
+import { useField, useFieldError } from 'vee-validate';
 
 const props = defineProps<{
-  allowedFiles: string;
-  infoAllowedFile: string;
-  hasError?: boolean;
-  errorMessage?: string;
+  id: string;
+  infoAllowedFile?: string;
   disabled?: boolean;
-  maxSize?: number;
 }>();
 
-const emit = defineEmits(['file-selected', 'file-upload-error']);
+const emit = defineEmits(['file-selected']);
 
 const fileInput = ref<HTMLElement | null>(null);
 const labelFileInput = ref<HTMLElement | null>(null);
 const fileName = ref();
 const isDragging = ref(false);
-const isNotValidFile = ref(false);
-const isNotValidFileMessage = ref('');
+const id = toRef(props, 'id');
+
+const { value, errorMessage, handleBlur, resetField } = useField(id);
+const hasErrorMessage = useFieldError(id);
 
 onMounted(() => {
-  isNotValidFile.value = false;
   isDragging.value = false;
 });
 
 const showFileName = computed(() => fileName.value);
 
 const labelClass = computed(() => {
-  if (props.hasError || isNotValidFile.value)
-    return 'border-red-300 hover:border-red-300';
+  if (hasErrorMessage.value) return 'border-red-300 hover:border-red-300';
   if (props.disabled) return 'border-grey-300 opacity-80 bg-grey-50';
   return 'border-green-600 hover:border-green-300';
 });
 
 function triggerFileInput(e: MouseEvent) {
-  isNotValidFile.value = false;
+  resetField();
   fileInput.value?.click();
   (e.target as HTMLElement)?.blur();
 }
 
-function fileValidation(file: File) {
-  const allowedFileTypes = props.allowedFiles
-    .split(',')
-    .map((type) => type.trim());
-
-  if (!allowedFileTypes.includes(file.type)) {
-    isNotValidFile.value = true;
-    isNotValidFileMessage.value = `${file.type} files are not allowed`;
-    return emit('file-upload-error', file);
-  } else if (props.maxSize && file.size > props.maxSize) {
-    isNotValidFile.value = true;
-    isNotValidFileMessage.value = `The file is too big. The maximum allowed size is ${convertBytes(props.maxSize)}`;
-    return emit('file-upload-error', file);
-  } else {
-    fileName.value = file.name;
-    return emit('file-selected', file);
-  }
+function fileUpload(file: File) {
+  fileName.value = file.name;
+  value.value = file;
+  return emit('file-selected', file);
 }
 
 function handleDragOver(event: DragEvent) {
   event.preventDefault();
-  isNotValidFile.value = false;
-
   labelFileInput.value?.classList.add('bg-green-50');
   isDragging.value = true;
 }
 
 function handleDragLeave() {
   isDragging.value = false;
-  isNotValidFile.value = false;
-
   labelFileInput.value?.classList.remove('bg-green-50');
 }
 
 function handleDrop(event: DragEvent) {
   event.preventDefault();
-  isNotValidFile.value = false;
   isDragging.value = false;
 
   if (event.dataTransfer?.files) {
     const file = event.dataTransfer.files[0];
-    fileValidation(file);
-
+    fileUpload(file);
     labelFileInput.value?.classList.remove('bg-green-50');
   }
 }
 
 function handleFileRemove(e: MouseEvent) {
   (e.target as HTMLElement)?.blur();
-  isNotValidFile.value = false;
 
   if (fileInput.value) {
     (fileInput.value as HTMLInputElement).value = '';
     fileName.value = null;
   }
+  resetField();
 }
 
 function handleFileChange(event: Event) {
-  isNotValidFile.value = false;
+  resetField();
   const input = event.target as HTMLInputElement;
   if (input.files?.length) {
     const file = input.files[0];
-    fileValidation(file);
+    fileUpload(file);
   }
 }
 </script>
