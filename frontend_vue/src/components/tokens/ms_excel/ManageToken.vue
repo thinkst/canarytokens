036f6ecb77363cc4e79aae36cf5c6bd71264ef5d
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-17 19:15:56 +0200

    Add Word and Excel tokens (#393)
    * add word and excel tokens
    
    * fix word type naming
    
    * remove validation string

---
+++ frontend_vue/src/components/tokens/ms_excel/ManageToken.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="!tokenData">Error loading</div>
+  <TokenDisplay
+    v-else
+    :token-data="tokenData"
+  />
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import TokenDisplay from './TokenDisplay.vue';
+import type { ManageTokenBackendType } from '@/components/tokens/types.ts';
+
+const props = defineProps<{
+  tokenBackendResponse: ManageTokenBackendType;
+}>();
+
+const tokenData = ref({
+  token: props.tokenBackendResponse?.canarydrop?.canarytoken?._value || '',
+  auth: props.tokenBackendResponse.canarydrop?.auth || '',
+});
+</script>
