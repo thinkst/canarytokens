eb29cf9cfcc4f1684e0b8999f1128aec6df63ebf
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-17 19:06:20 +0200

    Generate custom image web bug (#390)
    * first iteration
    
    * fix validation, tests
    
    * update validators add manage token
    
    * removed unused component declaration:
    
    * hide wise man
    
    * remove validation, move input under note

---
+++ frontend_vue/src/components/tokens/web_image/GenerateTokenForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <BaseFormTextField
+    id="email"
+    type="text"
+    placeholder="Email"
+    label="Email"
+    full-width
+    helper-message="Provide an email address or webhook URL"
+  />
+  <BaseFormTextField
+    id="webhook_url"
+    type="text"
+    placeholder="URL"
+    label="URL"
+    full-width
+    helper-message="Provide an email address or webhook URL"
+  />
+  <BaseFormTextField
+    id="memo"
+    label="Add Note"
+    multiline
+    required
+    full-width
+    helper-message="Reminder note when this token is triggered. For instance: Image embedded in router X's admin interface"
+  />
+  <BaseUploadFile
+    id="web_image"
+    info-allowed-file="JPG, JPEG, GIF or PNG"
+  />
+</template>
+
+<script setup lang="ts"></script>
