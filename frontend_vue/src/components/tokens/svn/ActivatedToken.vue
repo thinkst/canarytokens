63acbae872e36219eb88332aa3c947d34cf09c38
Vittoria Toso <vittoria@thinkst.com>
2024-04-17 19:36:49 +0200

    Add SVN token

---
+++ frontend_vue/src/components/tokens/svn/ActivatedToken.vue
@@ -0,0 +1,37 @@
+<template>
+  <TokenDisplay :token-data="tokenCode" />
+  <p class="mt-16 text-sm">
+    Remember, it gets triggered whenever someone clones the SVN repo.
+  </p>
+  <base-message-box
+    class="mt-24"
+    variant="warning"
+    :message="`Don't forget to run the following command after you've added the token:`"
+  />
+  <BaseCodeSnippet
+    class="mt-16"
+    lang="bash"
+    code="svn commit"
+  ></BaseCodeSnippet>
+  <p class="mt-24 text-sm">
+    The source IP address shown in the alert is the DNS server, not the end
+    user.
+  </p>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import TokenDisplay from './TokenDisplay.vue';
+import type { NewTokenBackendType } from '@/components/tokens/types';
+import generateSVNToken from './generateSVNToken.ts';
+
+const props = defineProps<{
+  tokenData: NewTokenBackendType;
+}>();
+
+const tokenHostname = ref(props.tokenData.hostname);
+
+const tokenCode = computed(
+  () => tokenHostname.value && generateSVNToken(tokenHostname.value)
+);
+</script>
---
+++ frontend_vue/src/components/tokens/svn/TokenDisplay.vue
@@ -0,0 +1,13 @@
+<template>
+  <base-code-snippet
+    lang="bash"
+    label="SVN command"
+    :code="tokenData"
+  ></base-code-snippet>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  tokenData: string;
+}>();
+</script>
