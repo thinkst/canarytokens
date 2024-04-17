bc46f22b65671b71f4e04c7976ae30b45a2b1c46
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-17 19:15:56 +0200

    Add Word and Excel tokens (#393)
    * add word and excel tokens
    
    * fix word type naming
    
    * remove validation string

---
+++ frontend_vue/src/components/tokens/ms_word/TokenDisplay.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex justify-center">
+    <base-button
+      class="mt-16"
+      @click="handleDownloadWordDocument"
+      >Download your MS Word file</base-button
+    >
+  </div>
+</template>
+<script setup lang="ts">
+import { downloadAsset } from '@/api/main';
+
+type MSExcelDataType = {
+  auth: string;
+  token: string;
+};
+const props = defineProps<{
+  tokenData: MSExcelDataType;
+}>();
+
+async function handleDownloadWordDocument() {
+  const params = {
+    fmt: 'msword',
+    auth: props.tokenData?.auth,
+    token: props.tokenData?.token,
+  };
+  try {
+    const res = await downloadAsset(params);
+    window.location.href = res.request.responseURL;
+  } catch (err) {
+    console.log(err, 'File download failed');
+  } finally {
+    console.log('Donwload ready');
+  }
+}
+</script>
