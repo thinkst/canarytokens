60faccf89218311557c08e4a07b42907ed56342b
Vittoria Toso <vittoria@thinkst.com>
2024-04-17 17:22:00 +0200

    add word and excel tokens

---
+++ frontend_vue/src/components/tokens/ms_excel/ActivatedToken.vue
@@ -0,0 +1,27 @@
+<template>
+  <TokenDisplay :token-data="tokenSnippetData" />
+  <p class="mt-24 text-sm">
+    You'll get an alert whenever this document is opened in Microsoft Office, on
+    Windows or Mac OS.
+  </p>
+  <base-message-box
+    class="mt-24"
+    variant="info"
+    message="You can rename the document without affecting its operation."
+  />
+</template>
+
+<script setup lang="ts">
+import TokenDisplay from './TokenDisplay.vue';
+import { ref } from 'vue';
+import type { NewTokenBackendType } from '@/components/tokens/types';
+
+const props = defineProps<{
+  tokenData: NewTokenBackendType;
+}>();
+
+const tokenSnippetData = ref({
+  token: props.tokenData.token || '',
+  auth: props.tokenData.auth_token || '',
+});
+</script>
