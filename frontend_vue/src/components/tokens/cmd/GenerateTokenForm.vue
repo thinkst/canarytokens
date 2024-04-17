c2365d6fdb834f0788e08fcc7f22d92004abcd45
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-17 17:36:06 +0200

    add sensitive command (#392)

---
+++ frontend_vue/src/components/tokens/cmd/GenerateTokenForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <BaseFormTextField
+    id="email"
+    type="text"
+    placeholder="Email"
+    label="Email"
+    full-width
+    helper-message="Provide an email address or webhook URL"
+  />
+  <BaseFormTextField
+    id="webhook_url"
+    type="text"
+    placeholder="URL"
+    label="URL"
+    full-width
+    helper-message="Provide an email address or webhook URL"
+  />
+  <div class="relative">
+    <BaseFormTextField
+      id="cmd_process"
+      type="text"
+      placeholder="File name"
+      label="Name of the process to monitor"
+      helper-message="Add a .exe exstension, e.g klist.exe"
+      full-width
+    />
+  </div>
+  <BaseFormTextField
+    id="memo"
+    label="Add Note"
+    multiline
+    required
+    full-width
+    helper-message="Reminder note when this token is triggered. For instance: AWS keys placed on Jim's laptop"
+  ></BaseFormTextField>
+</template>
+
+<script setup lang="ts"></script>
