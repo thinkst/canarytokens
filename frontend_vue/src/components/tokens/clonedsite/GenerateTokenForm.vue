61ef3f86fb979d730e78e17cf38956ecf94bad1d
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-18 20:39:11 +0200

    Generate cloned website (#403)
    * WIP-get-cloned-website-snippet
    
    * comment out broken stuff

---
+++ frontend_vue/src/components/tokens/clonedsite/GenerateTokenForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <BaseFormTextField
+    id="email"
+    type="text"
+    placeholder="Email"
+    label="Email"
+    full-width
+    helper-message="Provide an email address or webhook URL"
+  />
+  <BaseFormTextField
+    id="webhook_url"
+    type="text"
+    placeholder="URL"
+    label="URL"
+    full-width
+    helper-message="Provide an email address or webhook URL"
+  />
+  <BaseFormTextField
+    id="clonedsite"
+    type="text"
+    placeholder="e.g. thinkst.com"
+    label="Domain of protected website"
+    full-width
+  />
+  <BaseFormTextField
+    id="memo"
+    label="Add Note"
+    multiline
+    required
+    full-width
+    helper-message="Reminder note when this token is triggered. For instance: Cloned website token for https://thinkst.com"
+  />
+</template>
+
+<script setup lang="ts"></script>
