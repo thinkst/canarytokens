4aa97bd25bd16fe9451a515f3a8d1be782dc05b8
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-18 20:47:42 +0200

    Generate CSS Cloned website (#401)
    * Generate CSS cloned website
    
    * fix after rebasing

---
+++ frontend_vue/src/components/tokens/cssclonedsite/TokenDisplay.vue
@@ -0,0 +1,15 @@
+<template>
+  <base-code-snippet
+    lang="css"
+    label="CSS token"
+    :code="tokenSnippet"
+    multiline
+    custom-height="8rem"
+  ></base-code-snippet>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  tokenSnippet: string;
+}>();
+</script>
