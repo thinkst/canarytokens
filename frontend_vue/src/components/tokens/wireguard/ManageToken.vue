acc4457522046797b95f663f62c2f0f578eee815
Vittoria Toso <vittoria@thinkst.com>
2024-04-18 22:31:50 +0200

    Add Wireguard VPN

---
+++ frontend_vue/src/components/tokens/wireguard/ManageToken.vue
@@ -0,0 +1,18 @@
+<template>
+  <TokenDisplay :token-data="tokenData" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import type { ManageTokenBackendType } from '@/components/tokens/types.ts';
+import TokenDisplay from './TokenDisplay.vue';
+
+const props = defineProps<{
+  tokenBackendResponse: ManageTokenBackendType;
+}>();
+
+const tokenData = ref({
+  qr_code: props.tokenBackendResponse?.wg_qr_code,
+  wg_conf: props.tokenBackendResponse?.wg_conf,
+});
+</script>
