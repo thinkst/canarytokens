527d4ab744416fff38539d8b311446ad8c9d8bf6
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-18 17:23:39 +0200

    generate windows folder ZIP (#400)
    * generate windows folder ZIP
    
    * removed unused data

---
+++ frontend_vue/src/components/tokens/windows_dir/TokenDisplay.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex justify-center">
+    <base-button
+      class="mt-16"
+      @click="handleDownloadWindowsFolderFile"
+      >Download your ZIP file</base-button
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+import { downloadAsset } from '@/api/main';
+
+type WindowsFolderDataType = {
+  auth: string;
+  token: string;
+};
+
+const props = defineProps<{
+  tokenData: WindowsFolderDataType;
+}>();
+
+async function handleDownloadWindowsFolderFile() {
+  const params = {
+    fmt: 'zip',
+    auth: props.tokenData?.auth,
+    token: props.tokenData?.token,
+  };
+  try {
+    const res = await downloadAsset(params);
+    window.location.href = res.request.responseURL;
+  } catch (err) {
+    console.log(err, 'File download failed');
+  } finally {
+    console.log('Donwload ready');
+  }
+}
+</script>
