a1ee99b54d788e84b3dd22dacc24abc8b0ed17ea
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-18 09:27:38 +0200

    add unique email address (#394)

---
+++ frontend_vue/src/components/tokens/smtp/GenerateTokenForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <BaseFormTextField
+    id="email"
+    type="text"
+    placeholder="Email"
+    label="Email"
+    full-width
+    helper-message="Provide an email address or webhook URL"
+  />
+  <BaseFormTextField
+    id="webhook_url"
+    type="text"
+    placeholder="URL"
+    label="URL"
+    full-width
+    helper-message="Provide an email address or webhook URL"
+  />
+  <BaseFormTextField
+    id="memo"
+    label="Add Note"
+    multiline
+    required
+    full-width
+    helper-message="Reminder note when this token is triggered, like: Email address in Events database"
+  ></BaseFormTextField>
+</template>
+
+<script setup lang="ts"></script>
