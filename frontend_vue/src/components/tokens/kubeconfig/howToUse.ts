3006f627a9b938724a99e5e2ba6d965a9e88492d
Vittoria Toso <vittoria@thinkst.com>
2024-04-18 22:51:38 +0200

    generate-kubeconfig-token

---
+++ frontend_vue/src/components/tokens/kubeconfig/howToUse.ts
@@ -0,0 +1,4 @@
+export const howToUse = [
+  'Place the file at ~/.kube/config on a host, tempting an attacker to use it.',
+  'Place the file in private code repositories. If the token is triggered, it means that someone is accessing that repo without permission.',
+];
