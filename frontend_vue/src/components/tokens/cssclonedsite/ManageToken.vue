32aa5f2b044523a267490622bf04ac52dde8e8b0
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-18 20:47:42 +0200

    Generate CSS Cloned website (#401)
    * Generate CSS cloned website
    
    * fix after rebasing

---
+++ frontend_vue/src/components/tokens/cssclonedsite/ManageToken.vue
@@ -0,0 +1,24 @@
+<!-- TODO: to fix once backend is adjusted and /manage returns the CSS snippet -->
+<template>
+  <!-- <div v-if="!tokenSnippet">Error loading</div>
+  <TokenDisplay
+    v-else
+    :token-snippet="tokenSnippet"
+  /> -->
+  <BaseMessageBox variant="warning"
+    >The CSS snippet is not available at the moment. We'll fix this
+    soon.</BaseMessageBox
+  >
+</template>
+
+<script lang="ts" setup>
+// import { ref } from 'vue';
+// import TokenDisplay from './TokenDisplay.vue';
+// import type { ManageTokenBackendType } from '@/components/tokens/types.ts';
+
+// const props = defineProps<{
+//   tokenBackendResponse: ManageTokenBackendType;
+// }>();
+
+// const tokenSnippet = ref(props.tokenBackendResponse.canarydrop?.css);
+</script>
