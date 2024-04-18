5118db565638bac4ccb075721df1f0ea75fe6205
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-18 15:14:29 +0200

    generate sql server (#396)

---
+++ frontend_vue/src/components/tokens/sql_server/TokenDisplay.vue
@@ -0,0 +1,30 @@
+<template>
+  <base-code-snippet
+    lang="sql"
+    label="Microsoft Server SQL token"
+    :code="snippetCode"
+    multiline
+    custom-height="10rem"
+  ></base-code-snippet>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import generateSQLServertoken from './generateSQLServertoken';
+import type { SQLServerDataType } from './types';
+
+const props = defineProps<{
+  tokenData: SQLServerDataType;
+}>();
+
+const SQLData = ref({
+  hostname: props.tokenData.hostname,
+  sql_table_name: props.tokenData.sql_table_name,
+  sql_trigger_name: props.tokenData.sql_trigger_name,
+  sql_function_name: props.tokenData.sql_function_name,
+  sql_server_view_name: props.tokenData.sql_server_view_name,
+  sql_action: props.tokenData.sql_action,
+});
+
+const snippetCode = generateSQLServertoken(SQLData.value);
+</script>
