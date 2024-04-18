7ca1db638b4e50a2d879187e1a962553d48b535f
Vittoria Toso <vittoria@thinkst.com>
2024-04-18 20:27:57 +0200

    comment out broken stuff

--- frontend_vue/src/components/tokens/clonedsite/TokenDisplay.vue
+++ frontend_vue/src/components/tokens/clonedsite/TokenDisplay.vue
@@ -1,3 +1,6 @@
+<!-- TODO: the JavascriptObfuscator package is not working at the moment 
+we need to fix this dependency to show the obfuscated code
+-->
 <template>
   <base-code-snippet
     lang="javascript"
@@ -5,32 +8,33 @@
     multiline
     custom-height="8rem"
     :code="codeClonedSite"
-  ></base-code-snippet>
-  <base-switch
+  >
+  </base-code-snippet>
+  <!-- <base-switch
     id="obfuscated_code"
     v-model="obfuscated_code"
     class="mt-16"
     label="Obfuscate this script"
     helper-message="Show Obfuscated JavaScript"
     @input.stop="handleEncodingChange()"
-  ></base-switch>
+  ></base-switch> -->
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import type { Ref } from 'vue';
-import obfuscateToken from './obfuscateToken';
+// import type { Ref } from 'vue';
+// import obfuscateToken from './obfuscateToken';
 
 const props = defineProps<{
   tokenSnippet: string;
 }>();
 
 const codeClonedSite = ref(props.tokenSnippet);
-const obfuscated_code: Ref<boolean> = ref(false);
+// const obfuscated_code: Ref<boolean> = ref(false);
 
-function handleEncodingChange() {
-  obfuscated_code.value = !obfuscated_code.value;
-  if (obfuscated_code.value === true)
-    codeClonedSite.value = obfuscateToken(props.tokenSnippet);
-}
+// function handleEncodingChange() {
+//   obfuscated_code.value = !obfuscated_code.value;
+//   if (obfuscated_code.value === true)
+//     codeClonedSite.value = obfuscateToken(props.tokenSnippet);
+// }
 </script>
