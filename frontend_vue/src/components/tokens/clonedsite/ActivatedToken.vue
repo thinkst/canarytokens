b3e9c020d6add9b8ac18a26b6c6e96eeccec7b31
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-18 20:39:11 +0200

    Generate cloned website (#403)
    * WIP-get-cloned-website-snippet
    
    * comment out broken stuff

---
+++ frontend_vue/src/components/tokens/clonedsite/ActivatedToken.vue
@@ -0,0 +1,23 @@
+<template>
+  <TokenDisplay :token-snippet="tokenSnippetData" />
+  <p class="mt-16 text-sm">
+    Use this Javascript to detect when someone has cloned a webpage.
+  </p>
+  <base-message-box
+    class="mt-24"
+    variant="info"
+    message="When someone clones your site, they’ll grab this JavaScript too. When the script runs on their cloned site, it triggers an alert to let you know what’s going on."
+  />
+</template>
+
+<script setup lang="ts">
+import TokenDisplay from './TokenDisplay.vue';
+import { ref } from 'vue';
+import type { NewTokenBackendType } from '@/components/tokens/types';
+
+const props = defineProps<{
+  tokenData: NewTokenBackendType;
+}>();
+
+const tokenSnippetData = ref(props.tokenData.clonedsite_js || '');
+</script>
