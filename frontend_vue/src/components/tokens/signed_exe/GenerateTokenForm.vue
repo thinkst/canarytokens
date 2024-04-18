9657300c8d695abe29f2b4c3aeb2aeb2069d6afc
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-18 17:17:05 +0200

    Generate-exe-custom-token (#398)

---
+++ frontend_vue/src/components/tokens/signed_exe/GenerateTokenForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <BaseFormTextField
+    id="email"
+    type="text"
+    placeholder="Email"
+    label="Email"
+    full-width
+    helper-message="Provide an email address or webhook URL"
+  />
+  <BaseFormTextField
+    id="webhook_url"
+    type="text"
+    placeholder="URL"
+    label="URL"
+    full-width
+    helper-message="Provide an email address or webhook URL"
+  />
+  <BaseFormTextField
+    id="memo"
+    label="Add Note"
+    multiline
+    required
+    full-width
+    helper-message="Reminder note when this token is triggered."
+  />
+  <BaseUploadFile
+    id="signed_exe"
+    info-allowed-file="EXE, DLL"
+  />
+</template>
+
+<script setup lang="ts"></script>
---
+++ frontend_vue/src/components/tokens/signed_exe/ManageToken.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="!tokenInfo">Error loading</div>
+  <!-- <TokenDisplay
+    v-else
+    :token-data="tokenInfo"
+  /> -->
+  <BaseMessageBox variant="warning">
+    Currently this token is not available for download, we will fix this
+    soon.</BaseMessageBox
+  >
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+// import TokenDisplay from './TokenDisplay.vue';
+import type { ManageTokenBackendType } from '@/components/tokens/types.ts';
+
+const props = defineProps<{
+  tokenBackendResponse: ManageTokenBackendType;
+}>();
+
+// TODO: these fields are coming empty from the backend
+const tokenInfo = ref({
+  file_name: props.tokenBackendResponse.canarydrop.file_name || '',
+  file_contents: props.tokenBackendResponse.canarydrop.file_contents || '',
+});
+</script>
