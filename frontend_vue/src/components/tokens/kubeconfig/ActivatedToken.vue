8dfb4a75a02034492866364daed6936cb69f2efc
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-04-19 10:10:18 +0200

    generate-kubeconfig-token (#405)
    * generate-kubeconfig-token
    
    * update token display data type

---
+++ frontend_vue/src/components/tokens/kubeconfig/ActivatedToken.vue
@@ -0,0 +1,21 @@
+<template>
+  <TokenDisplay :token-data="tokenSnippetData" />
+  <p class="mt-24 text-sm text-center">
+    You'll get an alert when someone tries to use your Kubeconfig.
+  </p>
+</template>
+
+<script setup lang="ts">
+import TokenDisplay from './TokenDisplay.vue';
+import { ref } from 'vue';
+import type { NewTokenBackendType } from '@/components/tokens/types';
+
+const props = defineProps<{
+  tokenData: NewTokenBackendType;
+}>();
+
+const tokenSnippetData = ref({
+  token: props.tokenData.token || '',
+  auth: props.tokenData.auth_token || '',
+});
+</script>
