9f0b4ab0e4dd547c8b0e4097f15648b2aeea861f
Vittoria Toso <vittoria@thinkst.com>
2024-04-19 11:11:43 +0200

    Generate azure entra id login
    Generate Azure Entra Login
    Handle exceptions
    Minor adjustment to BaseMessageBox component
    
    Notes: The Manual flow design is very much a placeholder.
    I will draw a better UI or just use the same one we have in production.

---
+++ frontend_vue/src/components/tokens/azure_id_config/ManageToken.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <TokenDisplay :token-data="tokenData" /> -->
+  <BaseMessageBox variant="warning"
+    >The token snippet is not available at the moment. We'll fix this
+    soon.</BaseMessageBox
+  >
+</template>
+
+<script lang="ts" setup>
+// import { ref } from 'vue';
+// import TokenDisplay from './TokenDisplay.vue';
+// import type { ManageTokenBackendType } from '@/components/tokens/types.ts';
+// const props = defineProps<{
+//   tokenBackendResponse: ManageTokenBackendType;
+// }>();
+</script>
