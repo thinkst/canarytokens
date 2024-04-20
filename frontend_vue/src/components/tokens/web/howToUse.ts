9153e698657ae8aaeb22d66b2e291db91bb04287
Max Mclaughlin <max@thinkst.com>
2024-04-20 15:05:20 +0200

    Fix copy

--- frontend_vue/src/components/tokens/web/howToUse.ts
+++ frontend_vue/src/components/tokens/web/howToUse.ts
@@ -1,6 +1,5 @@
 export const howToUse = [
   'In an email with a juicy subject line.',
   'Embedded in documents.',
-  'In an email with a juicy subject line.',
   'This URL is just an example. Apart from the hostname and the actual token (the random string), you can change all other parts of the URL.',
 ];
