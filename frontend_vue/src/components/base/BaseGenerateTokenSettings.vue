b3aba4d6e5245f6fd1806786cfa77f3fd2930dac
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-05-06 15:02:55 +0200

    Field email url restructure + minor UI fix (#420)
    * fix field/url generate forms + UI fixes for home page
    
    * fix hover on alert list by removing movign effect
    
    * update Skeleton generate component
    
    * Remove uppercase from Token names
    
    * Fix message validation
    
    * fix after review

---
+++ frontend_vue/src/components/base/BaseGenerateTokenSettings.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="relative border flex-1 group flex flex-col px-16 sm:px-24 pt-16 pb-24 bg-white rounded-xl top-[0px] shadow-solid-shadow-grey border-grey-200"
+  >
+    <h3 class="mb-16 text-sm font-semibold text-left text-grey-400">
+      {{ settingType }} Settings
+    </h3>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  settingType: 'Canarytoken' | 'Notifications';
+}>();
+</script>
--- frontend_vue/src/components/tokens/log4shell/GenerateTokenForm.vue
+++ frontend_vue/src/components/tokens/log4shell/GenerateTokenForm.vue
@@ -1,28 +1,9 @@
 <template>
-  <BaseFormTextField
-    id="email"
-    type="text"
-    placeholder="Email"
-    label="Email"
-    full-width
-    helper-message="Provide an email address or webhook URL"
+  <GenerateTokenSettingsNotifications
+    memo-helper-example=" Place where log line is inserted"
   />
-  <BaseFormTextField
-    id="webhook_url"
-    type="text"
-    placeholder="URL"
-    label="URL"
-    full-width
-    helper-message="Provide an email address or webhook URL"
-  />
-  <BaseFormTextField
-    id="memo"
-    label="Add Note"
-    multiline
-    required
-    full-width
-    helper-message="Reminder note when this token is triggered, like: Place where log line is inserted"
-  ></BaseFormTextField>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GenerateTokenSettingsNotifications from '@/components/ui/GenerateTokenSettingsNotifications.vue';
+</script>
