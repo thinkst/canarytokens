07301a97e97f6992f0e130a0bec378c3f6345df7
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-05-14 13:47:06 +0200

    Add new banner + minor fix (#426)
    * add new banner to modal + add banner to manage page + fix color old banner
    
    * display banner conditionally
    
    * fix paddings

--- frontend_vue/src/components/ModalToken.vue
+++ frontend_vue/src/components/ModalToken.vue
@@ -68,6 +68,15 @@
         >
       </template>
     </template>
+    <!-- banner ADV -->
+    <template #banner>
+      <BannerBirdCanarytools
+        v-if="
+          modalType === ModalType.NewToken || modalType === ModalType.HowToUse
+        "
+        class="mb-8"
+      />
+    </template>
   </BaseModal>
 </template>
 
@@ -80,6 +89,7 @@ import ModalContentActivatedToken from './ModalContentActivatedToken.vue';
 import ModalContentGenerateToken from './ModalContentGenerateToken.vue';
 import ModalContentGenerateTokenLoader from '@/components/ui/ModalContentGenerateTokenLoader.vue';
 import ModalContentActivatedTokenLoader from '@/components/ui/ModalContentActivatedTokenLoader.vue';
+import BannerBirdCanarytools from '@/components/ui/BannerBirdCanarytools.vue';
 import { generateToken } from '@/api/main';
 import { TOKENS_TYPE } from './constants';
 
