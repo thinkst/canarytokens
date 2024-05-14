e76048ecd5ab9bba3102ed89c14226b1c521e37b
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-05-14 17:32:50 +0200

    Fix header, Card Token, Carousel info, add Icons to Settings  (#427)
    * fix header, fix carousel, add icons to settings, fix card token size and alignment
    
    * add comment

--- frontend_vue/src/components/base/BaseGenerateTokenSettings.vue
+++ frontend_vue/src/components/base/BaseGenerateTokenSettings.vue
@@ -2,7 +2,14 @@
   <div
     class="relative border flex-1 group flex flex-col px-16 sm:px-24 pt-16 pb-24 bg-white rounded-xl top-[0px] shadow-solid-shadow-grey border-grey-200"
   >
-    <h3 class="mb-16 text-sm font-semibold text-left text-grey-400">
+    <h3
+      class="flex flex-row gap-8 mb-16 text-sm font-semibold text-left text-grey-400"
+    >
+      <font-awesome-icon
+        :icon="icon"
+        class="pt-4 text-grey-300"
+        aria-hidden="true"
+      ></font-awesome-icon>
       {{ settingType }} Settings
     </h3>
     <slot></slot>
@@ -10,7 +17,25 @@
 </template>
 
 <script setup lang="ts">
-defineProps<{
-  settingType: 'Canarytoken' | 'Notifications';
+import { computed } from 'vue';
+
+enum settingTypeEnum {
+  Canarytoken = 'Canarytoken',
+  Notifications = 'Notifications',
+}
+
+const props = defineProps<{
+  settingType: settingTypeEnum.Canarytoken | settingTypeEnum.Notifications;
 }>();
+
+const icon = computed(() => {
+  switch (props.settingType) {
+    case settingTypeEnum.Canarytoken:
+      return 'gear';
+    case settingTypeEnum.Notifications:
+      return 'bell';
+    default:
+      return 'gear';
+  }
+});
 </script>
