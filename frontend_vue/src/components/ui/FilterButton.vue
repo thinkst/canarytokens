fa844c60c108c00114f274a4d39dd4d3454ac32c
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-05-15 12:16:32 +0200

    Add search bar + Add filters (#428)
    * WIP search bar + filter
    
    * add debounce
    
    * add animations
    
    * add filter buttons, style buttons and minor fixes
    
    * fix categories, add filters
    
    * re-add commented code
    
    * fix after rebase
    
    * fix paddings for mobile
    
    * reduce paddings
    
    * earlier exit after if statement
    
    * remove shallow ref
    
    * remove import for shallowref
    
    * remove all shallowref

---
+++ frontend_vue/src/components/ui/FilterButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button
+    class="flex flex-col items-center px-16 border sm:py-8 sm:px-16 grayscale hover:grayscale-0 rounded-2xl hover:border-green-500 active:border-green-500 group"
+    :class="selected ? ' border-green-500 grayscale-0' : 'border-grey-200'"
+  >
+    <font-awesome-icon
+      v-if="selected"
+      icon="xmark"
+      class="absolute sm:right-[8px] left-[8px] top-[.25rem] w-2rem text-green-500"
+    ></font-awesome-icon>
+    <img
+      :src="filterLogo"
+      alt="Filter icon"
+      class="w-[2.5rem] not-sr-only group-hover:opacity-100 hidden sm:block"
+      :class="selected ? 'opacity-100' : 'opacity-75'"
+    />
+    <span
+      class="block capitalize sm:mt-8 group-hover:text-green-500 sm:p-0"
+      :class="selected ? 'text-green-500 pl-8' : 'text-grey-300 px-4'"
+      >{{ category }}</span
+    >
+  </button>
+</template>
+
+<script setup lang="ts">
+import getImageUrl from '@/utils/getImageUrl';
+
+const props = defineProps<{
+  icon: string;
+  category: string;
+  selected: boolean;
+}>();
+
+console.log(props.selected, 'selected');
+
+const filterLogo = getImageUrl(`token_icons/${props.icon}`);
+</script>
--- frontend_vue/src/views/HomeView.vue
+++ frontend_vue/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="my-24 text-center">
+  <div class="flex flex-col items-center gap-16 my-24 mb-[40px] text-center">
     <div>
       <h1 class="text-4xl font-semibold text-grey-900">
         Here goes something, like a
@@ -8,31 +8,66 @@
       <h2 class="mt-8 font-md text-grey-400">
         Something else we want to tell users when they land here.
       </h2>
+      <h3 class="mt-32 mb-16 text-xl text-grey-800">
+        Generate new Canarytoken
+      </h3>
     </div>
-    <h3 class="mt-32 text-xl text-grey-800">Generate Canarytoken</h3>
+    <SearchFilterTokensHeader
+      @filtered-list="filteredList = $event"
+      @filter-search="searchValue = $event"
+      @filter-category="filterValue = $event"
+    />
   </div>
+  <!--Tokens Grid -->
+  <!-- No elements found -->
+  <template v-if="Object.keys(filteredList).length === 0">
+    <p class="text-xl text-center text-grey-400">
+      Nothing found for "{{ searchValue }}"
+      {{ filterValue ? `in category "${filterValue}"` : '' }}
+    </p>
+  </template>
+
+  <!--Tokens Grid -->
+  <!-- Grid elements -->
   <AppLayoutGrid>
-    <template
-      v-for="(token, key) in tokenServices"
-      :key="key"
+    <TransitionGroup
+      :name="animationType"
+      class="relative"
     >
-      <CardToken
-        :title="token.label"
-        :description="token.description"
-        :logo-img-url="token.icon"
-        :documentation-link="token.documentationLink"
-        @click-token="() => handleClickToken(key as string)"
-      />
-    </template>
+      <template
+        v-for="(token, key) in filteredList"
+        :key="key"
+      >
+        <CardToken
+          :title="(token as TokenServiceType).label"
+          :description="(token as TokenServiceType).description"
+          :logo-img-url="(token as TokenServiceType).icon"
+          :documentation-link="(token as TokenServiceType).documentationLink"
+          @click-token="() => handleClickToken(key as string)"
+        />
+      </template>
+    </TransitionGroup>
   </AppLayoutGrid>
 </template>
 
 <script setup lang="ts">
+import { ref, watch } from 'vue';
+import type { Ref } from 'vue';
+import { tokenServices } from '@/utils/tokenServices';
+import type {
+  TokenServicesType,
+  TokenServiceType,
+} from '@/utils/tokenServices';
 import AppLayoutGrid from '@/layout/AppLayoutGrid.vue';
 import CardToken from '@/components/ui/CardToken.vue';
 import { useModal } from 'vue-final-modal';
 import ModalToken from '@/components/ModalToken.vue';
-import { tokenServices } from '@/utils/tokenServices';
+import SearchFilterTokensHeader from '@/components/SearchFilterTokensHeader.vue';
+
+const filterValue = ref('');
+const searchValue = ref('');
+const filteredList: Ref<TokenServicesType> = ref(tokenServices);
+const animationType = ref('move-grid');
 
 function handleClickToken(selectedToken: string) {
   const { open, close } = useModal({
@@ -44,4 +79,59 @@ function handleClickToken(selectedToken: string) {
   });
   open();
 }
+
+watch(filterValue, (newVal, oldVal) => {
+  if (oldVal === '') {
+    animationType.value = 'move-grid';
+  }
+  if (newVal === '') {
+    animationType.value = 'move-grid';
+  }
+  if (newVal !== '' && oldVal !== '') {
+    animationType.value = 'fade';
+  }
+});
 </script>
+
+<style scoped>
+.move-grid-enter {
+  opacity: 0;
+}
+
+.move-grid-enter-active {
+  transition: all 1s;
+}
+
+.move-grid-leave {
+  opacity: 0;
+}
+
+.move-grid-leave-active {
+  transition: all 0.3s;
+  opacity: 0;
+  transform: translateY(0);
+  position: absolute;
+}
+
+.move-grid-move {
+  transition: all 0.3s cubic-bezier(0.55, 0, 0.1, 1);
+}
+
+.fade-move,
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 1s cubic-bezier(0.55, 0, 0.1, 1);
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: translate(30px, 0);
+}
+
+.fade-leave-active {
+  opacity: 0;
+  transition: all 0.2s cubic-bezier(0.55, 0, 0.1, 1);
+  transform: translate(30px, 0);
+}
+</style>
