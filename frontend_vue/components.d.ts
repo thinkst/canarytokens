54ddd51965b7cf71e1aa29b4f67ff557180d91d7
Vittoria Toso <vittoria@thinkst.com>
2024-05-21 16:54:58 +0200

    remove not used base components

--- frontend_vue/components.d.ts
+++ frontend_vue/components.d.ts
@@ -7,27 +7,24 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    BaseButton: typeof import('./src/components/base/BaseButton.vue')['default']
-    BaseCodeSnippet: typeof import('./src/components/base/BaseCodeSnippet.vue')['default']
-    BaseCopyButton: typeof import('./src/components/base/BaseCopyButton.vue')['default']
-    BaseFormSelect: typeof import('./src/components/base/BaseFormSelect.vue')['default']
-    BaseFormTextField: typeof import('./src/components/base/BaseFormTextField.vue')['default']
-    BaseGenerateTokenSettings: typeof import('./src/components/base/BaseGenerateTokenSettings.vue')['default']
-    BaseLink: typeof import('./src/components/base/BaseLink.vue')['default']
-    BaseLinkDocumentation: typeof import('./src/components/base/BaseLinkDocumentation.vue')['default']
-    BaseMessageBox: typeof import('./src/components/base/BaseMessageBox.vue')['default']
-    BaseModal: typeof import('./src/components/base/BaseModal.vue')['default']
-    BaseNotificationBox: (typeof import('./src/components/base/BaseNotificationBox.vue'))['default']
-    BaseRadioInput: typeof import('./src/components/base/BaseRadioInput.vue')['default']
-    BaseRefreshButton: typeof import('./src/components/base/BaseRefreshButton.vue')['default']
-    BaseSkeletonLoader: typeof import('./src/components/base/BaseSkeletonLoader.vue')['default']
-    BaseSpinner: typeof import('./src/components/base/BaseSpinner.vue')['default']
-    BaseSVG: (typeof import('./src/components/base/BaseSVG.vue'))['default']
-    BaseSwitch: typeof import('./src/components/base/BaseSwitch.vue')['default']
-    BaseTextField: (typeof import('./src/components/base/BaseTextField.vue'))['default']
-    BaseUploadFile: typeof import('./src/components/base/BaseUploadFile.vue')['default']
-    BaseWarning: (typeof import('src/components/base/BaseWarning.vue'))['default']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
+    BaseButton: (typeof import('./src/components/base/BaseButton.vue'))['default'];
+    BaseCodeSnippet: (typeof import('./src/components/base/BaseCodeSnippet.vue'))['default'];
+    BaseCopyButton: (typeof import('./src/components/base/BaseCopyButton.vue'))['default'];
+    BaseFormSelect: (typeof import('./src/components/base/BaseFormSelect.vue'))['default'];
+    BaseFormTextField: (typeof import('./src/components/base/BaseFormTextField.vue'))['default'];
+    BaseGenerateTokenSettings: (typeof import('./src/components/base/BaseGenerateTokenSettings.vue'))['default'];
+    BaseLink: (typeof import('./src/components/base/BaseLink.vue'))['default'];
+    BaseLinkDocumentation: (typeof import('./src/components/base/BaseLinkDocumentation.vue'))['default'];
+    BaseMessageBox: (typeof import('./src/components/base/BaseMessageBox.vue'))['default'];
+    BaseModal: (typeof import('./src/components/base/BaseModal.vue'))['default'];
+    BaseRadioInput: (typeof import('./src/components/base/BaseRadioInput.vue'))['default'];
+    BaseRefreshButton: (typeof import('./src/components/base/BaseRefreshButton.vue'))['default'];
+    BaseSkeletonLoader: (typeof import('./src/components/base/BaseSkeletonLoader.vue'))['default'];
+    BaseSpinner: (typeof import('./src/components/base/BaseSpinner.vue'))['default'];
+    BaseSwitch: (typeof import('./src/components/base/BaseSwitch.vue'))['default'];
+    BaseTextField: (typeof import('./src/components/base/BaseTextField.vue'))['default'];
+    BaseUploadFile: (typeof import('./src/components/base/BaseUploadFile.vue'))['default'];
+    RouterLink: (typeof import('vue-router'))['RouterLink'];
+    RouterView: (typeof import('vue-router'))['RouterView'];
   }
 }
