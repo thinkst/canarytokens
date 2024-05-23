3c4b6fda77a1608018751c884da664b2e741b18d
thinkstpaul <145110595+thinkstpaul@users.noreply.github.com>
2024-05-23 12:02:16 +0300

    T8112 clonedsite obfuscator (#437)
    * Fix cloned site obfuscateToken
    
    * Center align show more button
    
    * Update types

--- frontend_vue/src/components/tokens/clonedsite/TokenDisplay.vue
+++ frontend_vue/src/components/tokens/clonedsite/TokenDisplay.vue
@@ -1,38 +1,37 @@
-<!-- TODO: the JavascriptObfuscator package is not working at the moment 
-we need to fix this dependency to show the obfuscated code
--->
 <template>
   <base-code-snippet
     lang="javascript"
     label="Your tokened Javascript"
     :code="codeClonedSite"
+    :show-expand-button="obfuscated_code"
+    :custom-height="'270px'"
   >
   </base-code-snippet>
-  <!-- <base-switch
+  <base-switch
     id="obfuscated_code"
     v-model="obfuscated_code"
     class="mt-16"
     label="Obfuscate this script"
-    helper-message="Show Obfuscated JavaScript"
+    :helper-message="`${obfuscated_code ? 'Hide' : 'Show'} Obfuscated JavaScript`"
     @input.stop="handleEncodingChange()"
-  ></base-switch> -->
+  ></base-switch>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-// import type { Ref } from 'vue';
-// import obfuscateToken from './obfuscateToken';
+import type { Ref } from 'vue';
+import obfuscateToken from './obfuscateToken';
 
 const props = defineProps<{
   tokenSnippet: string;
 }>();
 
 const codeClonedSite = ref(props.tokenSnippet);
-// const obfuscated_code: Ref<boolean> = ref(false);
+const obfuscated_code: Ref<boolean> = ref(false);
 
-// function handleEncodingChange() {
-//   obfuscated_code.value = !obfuscated_code.value;
-//   if (obfuscated_code.value === true)
-//     codeClonedSite.value = obfuscateToken(props.tokenSnippet);
-// }
+function handleEncodingChange() {
+  obfuscated_code.value = !obfuscated_code.value;
+  if (obfuscated_code.value === true) codeClonedSite.value = obfuscateToken(props.tokenSnippet);
+  else codeClonedSite.value = props.tokenSnippet
+}
 </script>
