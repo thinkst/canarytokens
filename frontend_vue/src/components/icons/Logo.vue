52b44e1246a414eab7ec667ecbdda3073b0dd7ae
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-05-24 13:36:13 +0200

    Mix - add smoother transitions, minor UI fix (#435)
    * Add favicon, button animations, fix card token animation, add incident token mobile button, fix focus style, fix alerts list download buttons
    
    * add SVG logo, add animation to SVG
    
    * remove unused code:
    
    * update logo code
    
    * cleanup animation
    
    * remove animations for motion reduced users

---
+++ frontend_vue/src/components/icons/Logo.vue
@@ -0,0 +1,115 @@
+<!-- eslint-disable vuejs-accessibility/no-static-element-interactions -->
+<!-- eslint-disable vuejs-accessibility/mouse-events-have-key-events -->
+<template>
+  <svg
+    xml:space="preserve"
+    shape-rendering="geometricPrecision"
+    text-rendering="geometricPrecision"
+    image-rendering="optimizeQuality"
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    viewBox="100 50 1750 450"
+    @mouseenter="handleStartAnimation"
+    @mouseleave="handleStopAnimation"
+  >
+    <g id="Layer_x0020_1">
+      <metadata id="CorelCorpID_0Corel-Layer" />
+      <g id="_2614003920496">
+        <path
+          fill="#234A51"
+          d="M1198 284c-8,0 -14,7 -14,15l0 14 173 0 0 29 -173 0 0 15c0,8 6,14 14,14l42 0 117 0 0 29 -159 0c-24,0 -43,-19 -43,-43l0 -58c0,-24 19,-44 43,-44l159 0 0 29 -159 0zm452 0c-8,0 -14,7 -14,15 0,8 6,14 14,14 40,0 80,0 120,0 24,0 44,20 44,44 0,24 -20,43 -44,43l-163 0 0 -29 163 0c8,0 15,-6 15,-14 0,-8 -7,-15 -15,-15l-120 0c-24,0 -43,-19 -43,-43 0,-24 19,-44 43,-44l164 0 0 29 -164 0zm-214 16c-3,-3 -6,-6 -10,-8 -3,-2 -7,-3 -11,-3l-2 0 0 111 -34 0 0 -145 36 0c10,0 18,2 24,5 7,3 13,8 19,15l70 81c3,3 6,5 10,7 3,2 7,4 11,4l2 0 0 -112 34 0 0 145 -36 0c-10,0 -19,-1 -26,-5 -7,-3 -13,-8 -19,-15l-68 -80zm-673 -16c-8,0 -15,7 -15,15l0 58c0,8 7,14 15,14 45,0 89,0 134,0 8,0 15,-6 15,-14l0 -58c0,-8 -7,-15 -15,-15 -45,0 -89,0 -134,0zm0 116c-24,0 -44,-19 -44,-43l0 -58c0,-24 20,-44 44,-44l134 0c24,0 44,20 44,44l0 58c0,24 -20,43 -44,43l-134 0zm-63 -145l0 29 -87 0 0 116 -29 0 0 -116 -87 0 0 -29c68,0 136,0 203,0zm262 0l29 0 0 58 69 0 58 -58 41 0 -72 73 72 72 -41 0 -58 -58 -69 0 0 58 -29 0c0,-48 0,-96 0,-145zm-75 -57l22 0c-4,-8 -10,-18 -15,-26 0,-1 -1,-2 -2,-2 -1,-1 -1,-1 -2,-1l-6 0c-1,0 -1,0 -2,1 -1,0 -2,1 -2,2 -5,8 -11,18 -15,26l22 0zm6 -43c5,0 9,2 11,6l38 65 -17 0c-2,-4 -5,-9 -7,-13l-31 0 -31 0c-2,4 -5,9 -7,13l-17 0 38 -65c2,-4 6,-6 11,-6l12 0zm182 0l-19 0 39 46 0 25 14 0 0 -25 38 -46 -18 0 -27 33 -27 -33zm-40 28c4,0 7,-3 7,-7 0,-4 -3,-7 -7,-7l-69 0 0 14 69 0zm5 43l-28 -28 -46 0 0 28 -14 0 0 -71c28,0 56,0 83,0 12,0 22,9 22,21 0,12 -10,22 -22,22l-3 0 28 28 -20 0zm-522 -57c-4,0 -7,3 -7,7l0 29c0,4 3,7 7,7l80 0 0 14 -80 0c-12,0 -21,-9 -21,-21l0 -29c0,-12 9,-21 21,-21l80 0 0 14 -80 0zm234 8c-1,-2 -2,-3 -4,-4 -2,-1 -4,-2 -6,-2l0 55 -14 0 0 -71 14 0c5,0 9,1 12,2 3,2 6,4 9,8l34 39c1,2 3,3 5,4 1,1 3,2 5,2l1 0 0 -55 14 0 0 71 -15 0c-5,0 -9,-1 -12,-2 -4,-2 -7,-4 -9,-8l-34 -39zm-89 21l22 0c-4,-8 -10,-18 -15,-26 0,-1 -1,-2 -2,-2 -1,-1 -1,-1 -2,-1l-6 0c-1,0 -1,0 -2,1 -1,0 -2,1 -2,2 -5,8 -11,18 -15,26l22 0zm6 -43c5,0 9,2 11,6l38 65 -17 0c-2,-4 -5,-9 -7,-13l-31 0 -31 0c-2,4 -5,9 -7,13l-17 0 38 -65c2,-4 6,-6 11,-6l12 0z"
+        />
+        <polygon
+          fill="#39D47D"
+          points="201,215 241,255 241,307 201,348 "
+        />
+        <polygon
+          fill="#39D47D"
+          points="141,408 141,155 181,195 181,368 "
+        />
+        <path
+          fill="#39D47D"
+          d="M256 241l-1 0 -40 -40 49 0c18,0 33,-6 46,-19l1 -1 -116 0 -40 -40 253 0c-22,21 -43,43 -64,64 -24,24 -54,36 -88,36z"
+        />
+        <polygon
+          fill="#39D47D"
+          points="215,362 255,322 307,322 348,362 "
+        />
+        <polygon
+          fill="#39D47D"
+          points="408,422 155,422 195,382 368,382 "
+        />
+        <path
+          fill="#39D47D"
+          d="M322 307l0 0 40 41 0 -49c0,-18 6,-33 19,-46l1 -1 0 116 40 40 0 -253c-21,21 -43,43 -64,64 -24,24 -36,54 -36,88z"
+        />
+        <path
+          fill="#EA4458"
+          d="M281 261c12,0 21,9 21,20 0,12 -9,21 -21,21 -11,0 -20,-9 -20,-21 0,-11 9,-20 20,-20z"
+        />
+        <circle
+          fill="#EA4458"
+          cx="281"
+          cy="276"
+          r="21"
+          class="logo-dot-front"
+        />
+        <rect fill="none" />
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script setup lang="ts">
+function handleStartAnimation() {
+  const circle = document.querySelector('.logo-dot-front');
+
+  // add <animate> elements to the circle
+  const animateR = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'animate'
+  );
+
+  // animate R element
+  const attributesSize = {
+    attributeName: 'r',
+    from: '21',
+    to: '100',
+    dur: '1000ms',
+    begin: '500ms',
+    repeatCount: 'indefinite',
+  };
+
+  Object.entries(attributesSize).forEach(([key, value]) => {
+    animateR.setAttribute(key, value);
+  });
+
+  circle.appendChild(animateR);
+
+  const animateOpacity = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'animate'
+  );
+
+  // animate opacity circle
+  const attributesOpacity = {
+    attributeName: 'opacity',
+    from: '1',
+    to: '0',
+    dur: '1000ms',
+    begin: '500ms',
+    repeatCount: 'indefinite',
+  };
+  Object.entries(attributesOpacity).forEach(([key, value]) => {
+    animateOpacity.setAttribute(key, value);
+  });
+
+  circle.appendChild(animateOpacity);
+}
+
+function handleStopAnimation() {
+  const circle = document.querySelector('.logo-dot-front');
+  // remove each animation element in the circle
+  Array.from(circle.childNodes).forEach((child) => child.remove());
+}
+</script>
--- frontend_vue/src/components/ui/AppLogo.vue
+++ frontend_vue/src/components/ui/AppLogo.vue
@@ -1,9 +1,12 @@
 <template>
-  <RouterLink to="/">
-    <img
-      src="../../assets/logo.png"
-      alt="Canary Tokens"
-      class="w-[12rem] min-w-[12rem]"
-    />
+  <RouterLink
+    to="/"
+    class="focus-visible:border-none focus-visible:outline-none focus-visible:ring-2 focus:ring-offset-2 focus:ring-green-500"
+  >
+    <Logo class="w-[15rem] min-w-[12rem]" />
   </RouterLink>
 </template>
+
+<script setup lang="ts">
+import Logo from '@/components/icons/Logo.vue';
+</script>
