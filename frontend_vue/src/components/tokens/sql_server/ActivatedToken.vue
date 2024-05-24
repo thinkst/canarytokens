78715a5e41c66817110c05621da0f3b8d8db21aa
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-05-24 17:37:47 +0200

    Add sql token display + fix view (#439)
    * add sql token display
    
    * remove comment

--- frontend_vue/src/components/tokens/sql_server/ActivatedToken.vue
+++ frontend_vue/src/components/tokens/sql_server/ActivatedToken.vue
@@ -1,9 +1,5 @@
-<!-- TODO: fix after related backend task on /generate is solved-->
 <template>
-  <BaseMessageBox variant="warning">
-    The token Snippet is Not available here at the moment. You can check it by
-    clicking on Manage Token below.
-  </BaseMessageBox>
+  <TokenDisplay :token-data="tokenCode" />
   <p class="mt-16 text-sm">
     When the actions are run, your Canarytoken will be triggered.
   </p>
@@ -14,10 +10,20 @@
 </template>
 
 <script setup lang="ts">
-// import TokenDisplay from './TokenDisplay.vue';
-// import type { NewTokenBackendType } from '@/components/tokens/types';
+import { ref } from 'vue';
+import TokenDisplay from './TokenDisplay.vue';
+import type { NewTokenBackendType } from '@/components/tokens/types';
 
-// const props = defineProps<{
-//   tokenData: NewTokenBackendType;
-// }>();
+const props = defineProps<{
+  tokenData: NewTokenBackendType;
+}>();
+
+const tokenCode = ref({
+  hostname: props.tokenData.hostname || '',
+  sql_table_name: props.tokenData.sql_server_table_name || '',
+  sql_trigger_name: props.tokenData.sql_server_trigger_name || '',
+  sql_function_name: props.tokenData?.sql_server_function_name || '',
+  sql_server_view_name: props.tokenData.sql_server_view_name || '',
+  sql_action: props.tokenData.sql_server_sql_action || '',
+});
 </script>
