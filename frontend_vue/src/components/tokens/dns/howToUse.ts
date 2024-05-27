c254190ad73678e90a1a99b2e731d139cd0560a0
Vittoria Toso <vittoria@thinkst.com>
2024-05-27 17:23:39 +0300

    Fix How To Use to show code

--- frontend_vue/src/components/tokens/dns/howToUse.ts
+++ frontend_vue/src/components/tokens/dns/howToUse.ts
@@ -1,6 +1,6 @@
 export const howToUse = [
   'Include in a PTR entry for dark IP space of your internal network. Quick way to determine if someone is walking your internal DNS without configuring DNS logging and monitoring.',
-  'Leave in a .bash_history, or .ssh/config, or ~/servers.txt',
-  'Use as a extremely simple bridge between a detection and notification action. Many possibilities, here`s one that tails a logfile and triggers the token when someone logs in: <pre>tail -f /var/log/auth.log | awk `/Accepted publickey for/ { system("host k5198sfh3cw64rhdpm29oo4ga.canarytokens.com") }`</pre>',
+  'Leave in a <code>.bash_history</code>, or  <code>.ssh/config</code>, or <code>~/servers.txt</code>',
+  'Use as a extremely simple bridge between a detection and notification action. Many possibilities, here`s one that tails a logfile and triggers the token when someone logs in: <code>tail -f /var/log/auth.log | awk `/Accepted publickey for/ { system("host k5198sfh3cw64rhdpm29oo4ga.canarytokens.com") }`</code>',
   'Use as the domain part of an email address.',
 ];
