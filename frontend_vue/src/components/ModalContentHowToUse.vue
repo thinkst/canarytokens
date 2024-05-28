ce8847fb25302a68ca75f11f2da0420246186131
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-05-28 14:20:38 +0300

    Fix 'How to use' string code support (#446)
    * Fix How To Use to show code
    
    * add padding and rounding

--- frontend_vue/src/components/ModalContentHowToUse.vue
+++ frontend_vue/src/components/ModalContentHowToUse.vue
@@ -1,27 +1,30 @@
 <template>
   <ModalContentHowToUseLoader v-if="isLoading" />
-  <div v-else>
+  <template v-else>
     <ul
       v-if="howToUseToken.length > 0"
-      class="flex flex-col gap-16 my-16 ml-16 items-left text-grey-800"
+      class="flex flex-col w-full gap-16 my-16 ml-16 items-left text-grey-800"
     >
       <li
-        v-for="item in howToUseToken"
-        :key="item"
+        v-for="item in parsedHowToUseToken"
+        :key="item.id"
         class="grid justify-start grid-flow-col gap-8 text-left"
       >
-        {{ item }}
+        <component
+          :is="item.component"
+          v-bind="item.props"
+        />
       </li>
     </ul>
     <div v-else>
       At this time, we do not have any suggestions available. Please check back
       soon!
     </div>
-  </div>
+  </template>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import ModalContentHowToUseLoader from '@/components/ui/ModalContentHowToUseLoader.vue';
 
 const props = defineProps<{
@@ -30,6 +33,18 @@ const props = defineProps<{
 
 const howToUseToken = ref([]);
 const isLoading = ref(true);
+
+const parsedHowToUseToken = computed(() => {
+  return howToUseToken.value.map((item, index) => {
+    return {
+      id: index,
+      component: 'p',
+      props: {
+        innerHTML: item,
+      },
+    };
+  });
+});
 
 const loadHowToUse = async () => {
   const { howToUse } = await import(
@@ -51,5 +66,9 @@ li::before {
   margin-top: 0.1rem;
   background-color: hsl(157, 77%, 45%);
   border: 4px solid hsl(141, 75%, 76%);
+}
+
+p >>> code {
+  @apply bg-grey-100 px-8 py-[2px] rounded-md mt-4;
 }
 </style>
--- frontend_vue/src/components/tokens/kubeconfig/howToUse.ts
+++ frontend_vue/src/components/tokens/kubeconfig/howToUse.ts
@@ -1,4 +1,4 @@
 export const howToUse = [
-  'Place the file at ~/.kube/config on a host, tempting an attacker to use it.',
+  'Place the file at <code>~/.kube/config</code> on a host, tempting an attacker to use it.',
   'Place the file in private code repositories. If the token is triggered, it means that someone is accessing that repo without permission.',
 ];
