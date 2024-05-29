003adb25506bb6282c3e54057c559b11dc1a11ec
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-05-29 13:12:41 +0300

    Set autofocus on search (#451)
    * add autofocus to search bar
    
    * add autofocus on searchbar + fix focuse visible

--- frontend_vue/src/components/ui/SearchBar.vue
+++ frontend_vue/src/components/ui/SearchBar.vue
@@ -7,11 +7,12 @@
     >
     <input
       id="search-canarytoken"
+      ref="searchbarRef"
       v-bind="$attrs"
       v-model="model"
       type="text"
       :placeholder="placeholder"
-      class="placeholder:font-thin py-8 pl-[2.5rem] border font-semibold resize-none shadow-inner-shadow-grey rounded-3xl border-grey-200 outline-offset-4"
+      class="placeholder:font-thin py-8 pl-[2.5rem] border font-semibold resize-none shadow-inner-shadow-grey rounded-3xl border-grey-200"
     />
     <font-awesome-icon
       icon="magnifying-glass"
@@ -21,12 +22,19 @@
 </template>
 
 <script lang="ts" setup>
+import { ref, onMounted } from 'vue';
 defineProps<{
   placeholder: string;
   label: string;
 }>();
 
 const model = defineModel<string>();
+
+const searchbarRef = ref();
+
+onMounted(() => {
+  searchbarRef.value.focus();
+});
 </script>
 
 <style scoped>
