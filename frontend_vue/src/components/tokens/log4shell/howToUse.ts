c6a0a63586f941df1162b7857f41eb2fc86e5867
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2024-05-30 11:53:39 +0300

    Add How to use Wireguard and log4shell (#455)
    * Add How to use for Wireguard token and log4shell
    
    * add fullstops

--- frontend_vue/src/components/tokens/log4shell/howToUse.ts
+++ frontend_vue/src/components/tokens/log4shell/howToUse.ts
@@ -1,1 +1,4 @@
-export const howToUse = [];
+export const howToUse = [
+'Enter the resultant string into web based search boxes and fields that will be parsed by your logging libraries.',
+'Add the string to HTTP Request Headers.',
+'Use the string in automated scanners.'];
