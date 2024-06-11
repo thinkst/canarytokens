f832385a94a0d22a1b9a7ca4c5d8ce2b5c12df90
thinkstpaul <145110595+thinkstpaul@users.noreply.github.com>
2024-06-11 12:54:19 +0300

    Improve the UX when selecting and deselecting filter categories (#483)

--- frontend_vue/src/components/ui/FilterButton.vue
+++ frontend_vue/src/components/ui/FilterButton.vue
@@ -1,16 +1,11 @@
 <template>
   <button
     class="relative flex flex-col items-center px-16 text-sm leading-relaxed duration-100 border rounded-2xl hover:border-green-500 active:border-green-500 group"
-    :class="selected ? ' border-green-500 ' : 'border-grey-200'"
+    :class="selected ? ' border-green-500 bg-green-100' : 'border-grey-200'"
   >
-    <font-awesome-icon
-      v-if="selected"
-      icon="xmark"
-      class="absolute left-[8px] top-[.25rem] w-2rem text-green-500"
-    ></font-awesome-icon>
     <span
       class="block capitalize group-hover:text-green-500"
-      :class="selected ? 'text-green-500 pl-8' : 'text-grey-300 px-4'"
+      :class="selected ? 'text-green-500 font-semibold' : 'text-grey-300'"
       >{{ category }}</span
     >
   </button>
