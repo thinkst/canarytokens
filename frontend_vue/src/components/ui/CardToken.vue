928714e9480bd66045366db7c9ada54ecd60417b
Vittoria Toso <vittoria@thinkst.com>
2024-06-12 13:14:30 +0200

    Add token card button, fix test for code snippet

--- frontend_vue/src/components/ui/CardToken.vue
+++ frontend_vue/src/components/ui/CardToken.vue
@@ -1,51 +1,42 @@
 <template>
-  <li class="relative flex token-card-wrapper">
-    <button
-      class="relative group border flex-1 group flex flex-col px-16 pt-16 pb-24 bg-white rounded-xl top-[0px] shadow-solid-shadow-grey border-grey-200 items-center duration-100 ease-in-out token-card"
-      @click.stop="handleClickToken"
+  <li class="relative flex flex-col token-card-wrapper">
+    <div
+      class="relative group border flex flex-col flex-1 group bg-white rounded-xl top-[0px] shadow-solid-shadow-grey border-grey-200 duration-100 ease-in-out token-card justify-between"
     >
-      <div v-if="isLoading">
-        <BaseSkeletonLoader
-          type="circle"
-          class="w-[4rem] h-[4rem]"
+      <!-- content -->
+      <div class="flex flex-col items-center px-16 pt-16 pb-24">
+        <div v-if="isLoading">
+          <BaseSkeletonLoader
+            type="circle"
+            class="w-[4rem] h-[4rem]"
+          />
+        </div>
+        <img
+          v-if="!isLoading"
+          :src="src"
+          class="h-[4.5rem] transition-transform duration-150 group-hover:scale-105"
+          aria-hidden="true"
+          :alt="`${title} logo`"
         />
+        <h3
+          class="flex-grow py-16 font-semibold leading-5 text-center text-grey-800"
+        >
+          {{ title }}
+        </h3>
+        <p
+          class="flex-grow text-sm leading-5 text-center text-grey-400 text-pretty"
+        >
+          {{ description }}
+        </p>
       </div>
-      <img
-        v-if="!isLoading"
-        :src="src"
-        class="h-[4rem] transition-transform duration-150 group-hover:scale-95"
-        aria-hidden="true"
-        :alt="`${title} logo`"
-      />
-      <span
-        class="flex-grow py-16 font-semibold leading-5 text-center text-grey-800"
+      <!--- cta --->
+      <button
+        class="w-full font-semibold bg-grey-50 text-grey-400 h-[3rem] rounded-b-xl transition duration-100 hover-card shadow-solid-shadow-grey card-button"
+        @click.stop="handleClickToken"
       >
-        {{ title }}
-      </span>
-      <span
-        class="flex-grow text-sm leading-5 text-center text-grey-400 text-pretty"
-      >
-        {{ description }}
-      </span>
-      <span
-        class="absolute border-none group-hover:right-[8px] group-focus:right-[8px] hover:right-[8px] focus:right-[8px] focus:opacity-100 py-8 px-8 focus:border opacity-0 right-[0] bottom-[0] group-hover:opacity-100 group-focus:opacity-100 transition-all"
-      >
-        <font-awesome-icon
-          icon="arrow-right"
-          class="hidden text-green-500 sm:block"
-          aria-hidden="true"
-        />
-        <span class="hidden fa-sr-only sm:block">Add {{ title }}</span>
-      </span>
-      <span
-        class="flex flex-row items-center gap-8 mt-24 font-semibold text-green-600 sm:hidden"
-        >Add Canarytoken
-        <font-awesome-icon
-          icon="arrow-right"
-          class="text-green-500"
-          aria-hidden="true"
-      /></span>
-    </button>
+        Create Token
+      </button>
+    </div>
 
     <BaseLinkDocumentation
       v-if="documentationLink"
@@ -105,7 +96,16 @@ watch(logoUrl, () => {
 
 <style scoped>
 .token-card:hover,
-.token-card:focus {
+.token-card:focus,
+.token-card:focus-within {
   @apply border-green shadow-solid-shadow-green-500-sm;
+}
+
+.card-button:focus {
+  @apply from-green to-green-200 text-white border-b-green shadow-solid-shadow-green-500-sm bg-gradient-to-b outline-none;
+}
+
+.hover-card {
+  @apply group-hover:from-green group-hover:to-green-200 group-hover:text-white group-hover:border-b-green group-hover:shadow-solid-shadow-green-500-sm group-hover:bg-gradient-to-b;
 }
 </style>
