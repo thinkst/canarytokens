fad5d3039105e8b098b94ed0bc8100102ff234ef
Vittoria Toso <vittoria@thinkst.com>
2024-06-18 16:03:18 +0200

    fix loader

--- frontend_vue/src/components/ui/ModalContentHowToUseLoader.vue
+++ frontend_vue/src/components/ui/ModalContentHowToUseLoader.vue
@@ -1,19 +1,21 @@
 <!-- Skeleton loader as Fallback for ModalContentHotWoUse-->
 <template>
   <div class="flex flex-col md:w-[100%] lg:w-[80%] sm:mt-40 px-8 w-full">
-    <ul class="flex flex-col w-full gap-16 my-16 mt-40 items-left">
+    <ul class="sm:grid sm:grid-cols-[1fr_1fr_1fr] flex flex-col">
       <li
         v-for="(item, i) in Array.from({ length: 3 })"
         :key="i"
-        class="flex flex-row items-center gap-16 sm:justify-start sm:flex-col sm:mt-0"
+        class="flex flex-row items-center flex-1 mt-16 sm:mt-0 sm:justify-start sm:flex-col"
       >
         <BaseSkeletonLoader
           class="h-[5rem] w-[5rem]"
           type="circle"
         />
-        <div class="w-[80%]">
+        <div
+          class="w-[80%] flex flex-col sm:items-center sm:ml-0 ml-24 sm:mt-16"
+        >
           <BaseSkeletonLoader
-            class="w-[80%] mt-24"
+            class="w-[80%]"
             type="text"
           />
           <BaseSkeletonLoader
