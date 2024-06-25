f520848e854bf14d549ef9b4d216a81da12caf5b
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2024-06-25 11:51:33 +0200

    Update syntax for MySQL token (#500)

--- frontend_vue/src/components/tokens/my_sql/generateMysqlToken.ts
+++ frontend_vue/src/components/tokens/my_sql/generateMysqlToken.ts
@@ -3,19 +3,19 @@ export default function generateMysqlToken(
   token: string,
   encoded: boolean
 ) {
-  const decodedCode = `SET @bb = CONCAT(\"CHANGE MASTER TO MASTER_PASSWORD='my-secret-pw', MASTER_RETRY_COUNT=1, MASTER_PORT=3306, MASTER_HOST='${hostname}', MASTER_USER='${token}\", @@lc_time_names, @@hostname, \"';\")`;
+  const decodedCode = `SET @bb = CONCAT(\"CHANGE REPLICATION SOURCE TO SOURCE_PASSWORD='my-secret-pw', SOURCE_RETRY_COUNT=1, SOURCE_PORT=3306, SOURCE_HOST='${hostname}', SOURCE_USER='${token}\", @@lc_time_names, @@hostname, \"';\")`;
   const encodedCode = btoa(
-    `SET @bb = CONCAT(\"CHANGE MASTER TO MASTER_PASSWORD='my-secret-pw', MASTER_RETRY_COUNT=1, MASTER_PORT=3306, MASTER_HOST='${hostname}', MASTER_USER='${token}\", @@lc_time_names, @@hostname, \"';\"`
+    `SET @bb = CONCAT(\"CHANGE REPLICATION SOURCE TO SOURCE_PASSWORD='my-secret-pw', SOURCE_RETRY_COUNT=1, SOURCE_PORT=3306, SOURCE_HOST='${hostname}', SOURCE_USER='${token}\", @@lc_time_names, @@hostname, \"';\"`
   );
   const code = encoded
-    ? `SET @b = ${encodedCode}; 
+    ? `SET @b = ${encodedCode};
 SET @s2 = FROM_BASE64(@b);
 PREPARE stmt1 FROM @s2;
 EXECUTE stmt1;
 PREPARE stmt2 FROM @bb;
 EXECUTE stmt2;
 START REPLICA;`
-    : `${decodedCode}; 
+    : `${decodedCode};
 PREPARE stmt FROM @bb;
 EXECUTE stmt;
 START REPLICA;`;
