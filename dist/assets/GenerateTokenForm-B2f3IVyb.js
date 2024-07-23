89207a8f3faa6174d314e54e5da8185983726d1c
Frontend Github Action <max@thinkst.com>
2024-07-23 11:07:57 +0000

    Add Frontend Dist

---
+++ dist/assets/GenerateTokenForm-B2f3IVyb.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./GenerateTokenSettingsNotifications.vue_vue_type_script_setup_true_lang-CnhKTSkZ.js";import{d as o,a as r,b as a}from"./index-k31JX26l.js";const c=o({__name:"GenerateTokenForm",setup(n){return(t,m)=>(r(),a(e,{"memo-helper-example":" Place where log line is inserted"}))}});export{c as default};
---
+++ dist/assets/GenerateTokenForm-nmUt8_iz.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./GenerateTokenSettingsNotifications.vue_vue_type_script_setup_true_lang-CnhKTSkZ.js";import{d as o,a as n,b as a}from"./index-k31JX26l.js";const s=o({__name:"GenerateTokenForm",setup(m){return(r,t)=>(n(),a(e,{"memo-helper-example":"MySQL token in payments_db_dump.sql on SRV01"}))}});export{s as default};
