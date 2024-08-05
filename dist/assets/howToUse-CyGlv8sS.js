d7aade01318d76a09ee10988f0f3b55466c52ecc
Frontend Github Action <max@thinkst.com>
2024-08-05 16:44:49 +0000

    Add Frontend Dist

---
+++ dist/assets/howToUse-CyGlv8sS.js
@@ -0,0 +1,1 @@
+const o=["Catch people snooping on your phone.","Have a decoy non-functional bank app.","If location is enabled, use it to email someone your current location."];export{o as howToUse};
