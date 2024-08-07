da2b6c8efe37f1bea1f9d1ca7d8fcdf0b09fa7ad
Frontend Github Action <max@thinkst.com>
2024-08-07 14:16:02 +0000

    Add Frontend Dist

---
+++ dist/assets/GenerateTokenForm-DxBUiSdk.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./GenerateTokenSettingsNotifications.vue_vue_type_script_setup_true_lang-9bBD9fSW.js";import{d as o,a as r,b as a}from"./index-CY40utSA.js";const s=o({__name:"GenerateTokenForm",setup(n){return(m,t)=>(r(),a(e,{"memo-helper-example":"SVN token on REPO01/ourmainrepo"}))}});export{s as default};
