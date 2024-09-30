99f0471cfa00efa087fd44bfb594ba4789a469bb
Frontend Github Action <max@thinkst.com>
2024-09-30 06:12:18 +0000

    Add Frontend Dist

---
+++ dist/assets/GenerateTokenForm-7qiqprZ2.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./GenerateTokenSettingsNotifications.vue_vue_type_script_setup_true_lang-BlvO7UfN.js";import{d as a,a as o,b as r}from"./index-CgoPuGfo.js";const p=a({__name:"GenerateTokenForm",setup(n){return(s,t)=>(o(),r(e,{"memo-helper-example":"Email address in Events database"}))}});export{p as default};
