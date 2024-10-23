0f51d23609be06d8f14f2fb99bd38880adc1564c
Frontend Github Action <max@thinkst.com>
2024-10-23 06:15:35 +0000

    Add Frontend Dist

---
+++ dist/assets/GenerateTokenForm-O5Mr-QLo.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./GenerateTokenSettingsNotifications.vue_vue_type_script_setup_true_lang-Cpjw0qmh.js";import{d as o,a,b as n}from"./index-HOUULui1.js";const _=o({__name:"GenerateTokenForm",setup(r){return(t,m)=>(a(),n(e,{"memo-helper-example":"WireGuard VPN config installed on Siya's phone"}))}});export{_ as default};
