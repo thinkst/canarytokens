4ec82397f48a09259d292e717ecc5bcae5d6f933
Frontend Github Action <max@thinkst.com>
2024-10-24 13:53:13 +0000

    Add Frontend Dist

---
+++ dist/assets/GenerateTokenForm-AFTBzTtK.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./GenerateTokenSettingsNotifications.vue_vue_type_script_setup_true_lang-lHu_yr5h.js";import{d as o,a,b as r}from"./index-CvZxS35p.js";const _=o({__name:"GenerateTokenForm",setup(m){return(n,p)=>(a(),r(e,{"memo-helper-example":"AWS keys placed on Jim's laptop"}))}});export{_ as default};
