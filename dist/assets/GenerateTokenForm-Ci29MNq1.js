1a46682b3f67d1cfd4bd8a627091297c53956e3c
Frontend Github Action <max@thinkst.com>
2024-10-24 14:27:20 +0000

    Add Frontend Dist

---
+++ dist/assets/GenerateTokenForm-Ci29MNq1.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./GenerateTokenSettingsNotifications.vue_vue_type_script_setup_true_lang-Bw0aZxJ_.js";import{d as o,a as r,b as a}from"./index-BGdoPI6i.js";const s=o({__name:"GenerateTokenForm",setup(n){return(t,m)=>(r(),a(e,{"memo-helper-example":"Kubeconfig token placed on web server WEB01"}))}});export{s as default};
