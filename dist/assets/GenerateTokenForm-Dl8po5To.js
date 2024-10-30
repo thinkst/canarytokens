ef84f1b38a07c1a60b980bf8165988a4a58347b6
Frontend Github Action <max@thinkst.com>
2024-10-30 16:24:54 +0000

    Add Frontend Dist

---
+++ dist/assets/GenerateTokenForm-Dl8po5To.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./GenerateTokenSettingsNotifications.vue_vue_type_script_setup_true_lang-CDWQq_wi.js";import{d as a,a as o,b as r}from"./index-Df32NadT.js";const p=a({__name:"GenerateTokenForm",setup(t){return(m,n)=>(o(),r(e,{"memo-helper-example":"Excel document placed at U:\\Users\\Max\\feb.xlxs"}))}});export{p as default};
