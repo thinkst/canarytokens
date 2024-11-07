43b7fc08b57b8ede4dbc677cc50a85d74192e40a
Vittoria Toso <vittoria@thinkst.com>
2024-11-07 10:22:19 +0100

    Fix alignments etc

--- frontend_vue/src/components/tokens/idp_app/CreateAppPreview.vue
+++ frontend_vue/src/components/tokens/idp_app/CreateAppPreview.vue
@@ -3,11 +3,11 @@
     class="flex flex-col gap-16 p-16 mt-24 mb-32 border border-grey-200 rounded-xl"
   >
     <h3 class="flex flex-row text-sm font-semibold text-left text-grey-400">
-      App Custom appareance
+      App appareance
     </h3>
     <base-form-select
       id="idp-app-look"
-      label="Select Custom App look"
+      label="Select App"
       :options="idpOptions"
       placeholder="Select an option"
       @select-option="handleSelectedApp"
@@ -35,63 +35,52 @@
       </template>
     </base-form-select>
 
-    <div class="p-16 bg-white rounded-xl">
-      <h3
-        class="flex flex-row gap-8 mb-16 text-sm font-semibold text-left text-grey-400"
+    <div class="flex flex-col w-full gap-24 p-16 bg-white rounded-xl">
+      <div class="flex flex-col items-center justify-center gap-8">
+        <BaseLabelArrow
+          id="idp-app-img"
+          :arrow-word-position="3"
+          label="Download App Icon"
+          arrow-variant="one"
+          class="text-center"
+        />
+        <BaseDownloadIconButtom
+          :url="selectedApp?.iconUrl && getImageUrl(`${selectedApp?.iconUrl}`)"
+          :disabled="!selectedApp?.iconUrl"
+        />
+      </div>
+      <div
+        class="flex flex-col items-center justify-center bg-white rounded-2xl"
       >
-        Preview
-      </h3>
-      <div class="grid grid-cols-1 gap-16 sm:gap-24">
-        <div class="flex flex-col items-center justify-center gap-8">
-          <BaseLabelArrow
-            id="idp-app-img"
-            :arrow-word-position="4"
-            label="Download custom App Icon"
-            arrow-variant="one"
-            class="text-center"
-          />
-          <BaseDownloadIconButtom
-            :url="
-              selectedApp?.iconUrl && getImageUrl(`${selectedApp?.iconUrl}`)
-            "
-            :disabled="!selectedApp?.iconUrl"
-          />
-        </div>
-        <div class="flex flex-col gap-8">
-          <BaseLabelArrow
-            id="idp-app-name"
-            :arrow-word-position="3"
-            label="Copy custom App name"
-            arrow-variant="two"
-            class="text-center"
-          />
-          <div
-            class="flex flex-row items-center justify-center gap-16 p-16 bg-white rounded-2xl"
+        <BaseLabelArrow
+          id="idp-app-name"
+          :arrow-word-position="3"
+          label="Copy App name"
+          arrow-variant="two"
+          class="z-10 text-center"
+        />
+        <!-- Copy Name Input Readonly -->
+        <div
+          class="relative flex flex-col w-full max-w-[40ch] items-center gap-8"
+        >
+          <label
+            for="text-input"
+            class="sr-only"
+            >App name</label
           >
-            <!-- Copy Name Input Readonly -->
-            <div
-              class="relative flex flex-col grow max-w-[60ch] items-center gap-8"
-            >
-              <label
-                for="text-input"
-                class="sr-only"
-                >App name</label
-              >
-              <input
-                id="text-input"
-                type="text"
-                placeholder="App name"
-                :value="selectedApp?.value"
-                readonly
-                class="h-[3rem] w-full px-16 py-8 border resize-none shadow-inner-shadow-grey rounded-3xl border-grey-400 focus:ring-green-600 focus-visible:ring-1"
-              />
-              <BaseCopyButton
-                :content="selectedApp?.value ? selectedApp?.value : ''"
-                :disabled="!selectedApp?.value"
-                class="absolute top-8 right-16"
-              />
-            </div>
-          </div>
+          <input
+            id="text-input"
+            type="text"
+            placeholder="App name"
+            :value="selectedApp?.value"
+            readonly
+            class="h-[3rem] w-full px-16 py-8 border resize-none shadow-inner-shadow-grey rounded-3xl border-grey-400 focus:ring-green-600 focus-visible:ring-1"
+          />
+          <BaseCopyButton
+            :content="selectedApp?.value ? selectedApp?.value : ''"
+            :disabled="!selectedApp?.value"
+            class="absolute top-8 right-16"
+          />
         </div>
       </div>
     </div>
