89b0fdee5f76cfb10d8c4f5518d7e8ef8ce654a2
William Leighton Dawson <leighton@thinkst.com>
2024-11-18 20:03:43 +0200

    Improve wording and link to docs

--- frontend_vue/src/components/tokens/idp_app/TokenDisplay.vue
+++ frontend_vue/src/components/tokens/idp_app/TokenDisplay.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col gap-16">
     <base-code-snippet
       lang="javascript"
-      label="Login URL"
+      label="Assertion Consumer Service / Relay State URL"
       is-single-line
       :code="tokenUrl"
     ></base-code-snippet>
@@ -13,6 +13,17 @@
       :code="entityId"
     ></base-code-snippet>
   </div>
+  <BaseMessageBox
+    variant="info"
+    class="mt-32"
+    >For setup instructions for Entra ID and Okta, head over to our
+    <a
+      style="text-decoration: underline"
+      href="https://docs.canarytokens.org/guide/idp-app-token.html"
+    >
+      docs
+    </a>
+  </BaseMessageBox>
 </template>
 
 <script setup lang="ts">
