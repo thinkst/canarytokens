24a8ad0f2156be9538a87caa7b97ab4835219ecc
Marco Slaviero <marco@thinkst.com>
2024-12-03 15:59:32 +0200

    Review fixes on the UI

--- frontend_vue/src/components/tokens/webdav/TokenDisplay.vue
+++ frontend_vue/src/components/tokens/webdav/TokenDisplay.vue
@@ -1,38 +1,74 @@
 <template>
+  <div>
+    <label class="mt-16 mb-8 text-grey-500 inline-block" >Here are the generic parameters:</label>
+    <div class="relative bg-white border rounded-2xl border-grey-100">
+      <ul>
+        <li>WebDAV server: <strong>{{NetworkFolder.webdav_server}}</strong><BaseCopyButton
+              :content="webdavServer"
+              class="ring-white ring-4 ml-8"
+            /></li>
+        <li>Username: Pick anything (e.g. <strong>service</strong>, <strong>Sabrina</strong>, or <strong>Admin</strong>)</li>
+        <li>Password: <strong>{{NetworkFolder.webdav_password}}</strong><BaseCopyButton
+              :content="webdavPassword"
+              class="ring-white ring-4 ml-8"
+            /></li>
+      </ul>
+    </div>
+  </div>
+  <label class="mt-16 mb-8 text-grey-500 inline-block" >On <strong>Windows</strong> systems, run these commands in cmd.exe:</label>
   <base-code-snippet
     lang="shell"
-    label="On Windows systems, run these commands in cmd.exe:"
     :code="snippetWindowsCode"
     custom-height="9.5rem"
   ></base-code-snippet>
   <div>
     <label class="mt-16 mb-8 text-grey-500 inline-block" >On <strong>MacOS</strong> systems, configure a Network Folder with these steps:</label>
-    <ul>
-      <li>Open the Finder, then type &#8984;-K to Connect to Server</li>
-      <li>Enter <strong>"{{NetworkFolder.webdav_server}}"</strong> as the hostname, and click Connect</li>
-      <li>Enter any username (e.g. "user")</li>
-      <li>Use <strong>{{NetworkFolder.webdav_password}}</strong> as the password</li>
-      <li>Optionally you can save the credentials into your Keychain if you want to remount at login. To remount at log, open System Settings -> General -> Open at Login, click the '+' to add a new item, navigate to the newly-mounted folder, and select it, then click Open.</li>
-    </ul>
+    <div class="relative bg-white border rounded-2xl border-grey-100 mr-2.5rem]">
+      <ul>
+        <li>Open the Finder, then type &#8984;-K to Connect to Server.</li>
+        <li>Copy and paste <span id="input-wrapper">
+            <input :value="webdavUrl" />
+            <BaseCopyButton
+              :content="webdavUrl"
+              class="ring-white ring-4"
+            />
+          </span> as the hostname, then click Connect.</li>
+        <li>Alternatively, to automatically mount the folder at login, use the "WebDAV server" field above as the hostname, then enter the username and password and check "Remember this password in my Keychain". Open System Settings -> General -> Open at Login, click the '+' to add a new item, navigate to the newly-mounted folder, and select it, then click Open.</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <style scoped>
 ul {
   list-style-type: disc;
+  padding: 1em;
 }
 
 ul li {
   margin-left: 1em;
   padding-left: .3em;
-  margin-bottom: 4px;
-  border-radius: 4px;
+}
+
+input {
+  text-overflow:ellipsis;
+  width: 12rem;
+}
+
+#input-wrapper {
+  outline: 2px solid hsl(162, 86%, 36%);
+  padding: .6rem;
+  font-family: monospace;
+}
+
+input:focus-visible {
+  outline: none;
 }
 </style>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import generateNetworkFoldertoken from './generateNetworkFolderToken';
+import { generateNetworkFoldertoken, generateWebdavUrl } from './generateNetworkFolderToken';
 import type { NetworkFolderDataType } from './types';
 
 const props = defineProps<{
@@ -47,4 +83,7 @@ const NetworkFolder = ref({
 });
 
 const snippetWindowsCode = generateNetworkFoldertoken(NetworkFolder.value);
+const webdavUrl = generateWebdavUrl(NetworkFolder.value);
+const webdavServer = NetworkFolder.value.webdav_server ? NetworkFolder.value.webdav_server : 'error';
+const webdavPassword = NetworkFolder.value.webdav_password ? NetworkFolder.value.webdav_password : 'error';;
 </script>
--- frontend_vue/src/components/tokens/webdav/generateNetworkFolderToken.ts
+++ frontend_vue/src/components/tokens/webdav/generateNetworkFolderToken.ts
@@ -1,6 +1,6 @@
 import type { NetworkFolderDataType } from './types';
 
-export default function generateNetworkFoldertoken(NetworkFolder: NetworkFolderDataType) {
+export function generateNetworkFoldertoken(NetworkFolder: NetworkFolderDataType) {
   const domain = NetworkFolder.webdav_server?.split("/")[2];
   const usageDescription = `# Run the next line once to save the credentials:
 cmdkey /generic:${domain} /user:user /pass:${NetworkFolder.webdav_password}
@@ -8,4 +8,9 @@ cmdkey /generic:${domain} /user:user /pass:${NetworkFolder.webdav_password}
 # On each boot run the line below in a startup script:
 net use * ${NetworkFolder.webdav_server} /savecred`
     return usageDescription;
+}
+
+export function generateWebdavUrl(NetworkFolder: NetworkFolderDataType) {
+    const domain = NetworkFolder.webdav_server?.split("/")[2];
+    return 'https://user:'+NetworkFolder.webdav_password+'@'+domain;
 }
