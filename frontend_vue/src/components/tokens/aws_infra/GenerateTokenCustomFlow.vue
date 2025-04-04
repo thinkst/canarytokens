f4a2bd754896b258e42bb673b67a1a53b7c55bb4
Vittoria Toso <vittoria@thinkst.com>
2025-04-04 15:58:55 +0200

    add steps and base component

--- frontend_vue/src/components/tokens/aws_infra/GenerateTokenCustomFlow.vue
+++ frontend_vue/src/components/tokens/aws_infra/GenerateTokenCustomFlow.vue
@@ -1,6 +1,45 @@
 <template>
-    <p>Test</p>
+  <BaseStepCounter
+    :steps="5"
+    :current-step="currentStep"
+  />
+  <component
+    :is="currentComponent"
+    @update-step="handleUpdateStep"
+  />
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from 'vue';
+import { defineAsyncComponent } from 'vue';
+
+const GenerateAwsSnippet = defineAsyncComponent(
+  () => import('./GenerateAwsSnippet.vue')
+);
+const CheckAwsRole = defineAsyncComponent(() => import('./CheckAwsRole.vue'));
+const InventoryAwsAccount = defineAsyncComponent(
+  () => import('./InventoryAwsAccount.vue')
+);
+const GeneratePlan = defineAsyncComponent(() => import('./GeneratePlan.vue'));
+const GenerateTerraformSnippet = defineAsyncComponent(
+  () => import('./GenerateTerraformSnippet.vue')
+);
+
+const currentStep = ref(1);
+
+const stepComponents = {
+  1: GenerateAwsSnippet,
+  2: CheckAwsRole,
+  3: InventoryAwsAccount,
+  4: GeneratePlan,
+  5: GenerateTerraformSnippet,
+};
+
+const currentComponent = computed(
+  () => stepComponents[currentStep.value] || null
+);
+
+function handleUpdateStep() {
+  currentStep.value++;
+}
 </script>
