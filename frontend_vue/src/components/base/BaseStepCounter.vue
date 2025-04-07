dd40edaf4a3514f08ae93dabc2e487946e5b9986
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2025-04-07 19:37:44 +0200

    Fontend update api static components (#658)
    * static components
    
    * add planner
    
    * fix ui
    
    * fix ts

--- frontend_vue/src/components/base/BaseStepCounter.vue
+++ frontend_vue/src/components/base/BaseStepCounter.vue
@@ -3,7 +3,7 @@
     <li
       v-for="(step, index) in props.steps"
       :key="step"
-      class="rounded-full w-[3rem] h-[3rem] mx-24 flex justify-center items-center"
+      class="rounded-full w-[2rem] h-[2rem] mx-24 flex justify-center items-center"
       :class="isActiveStep(index)"
     >
       <span class="text-white font-bold">{{ index + 1 }}</span>
@@ -17,7 +17,7 @@ const props = defineProps<{
   currentStep: number;
 }>();
 
-function isActiveStep(index) {
+function isActiveStep(index: number) {
   return index + 1 <= props.currentStep ? 'bg-green-700' : 'bg-grey-200';
 }
 </script>
