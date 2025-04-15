edb18d15db6d1ff51bd8165585d8053a0916873f
Vittoria Toso <vittoria@thinkst.com>
2025-04-15 10:53:05 +0200

    WIP

---
+++ frontend_vue/src/components/tokens/aws_infra/plan_generator/AssetCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <li class="relative flex token-card-wrapper">
+    <button
+      ref="assetCardRef"
+      class="group border flex flex-1 flex-col group bg-white rounded-2xl top-[0px] shadow-solid-shadow-grey border-grey-200 duration-100 ease-in-out justify-between asset-card items-center"
+      @click.stop="handleClickToken"
+      @mouseover="handleMouseOver"
+      @focus="handleMouseOver"
+      @mouseleave="handleMouseLeave"
+      @blur="handleMouseLeave"
+    >
+      <!-- Content -->
+      <div class="relative flex flex-col items-center gap-8 px-16 pt-16">
+        <p
+          class="flex-grow py-16 text-sm text-center text-grey-400 text-pretty"
+        >
+          Description
+        </p>
+      </div>
+      <!--- CTA text --->
+      <div
+        class="w-full leading-5 font-semibold border-t-2 border-grey-50 text-grey-700 h-[3rem] rounded-b-2xl transition duration-100 hover-card shadow-solid-shadow-grey card-button justify-center items-center flex px-8"
+      >
+        {{ isHoverCard ? 'Edit' : assetType }}
+      </div>
+    </button>
+  </li>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const emit = defineEmits(['clickAsset']);
+
+const props = defineProps<{
+  assetType: string;
+}>();
+
+const isHoverCard = ref(false);
+const assetCardRef = ref();
+
+function handleClickToken() {
+  emit('clickAsset');
+  // remove focus from selected card
+  if (assetCardRef.value) {
+    assetCardRef.value.blur();
+  }
+}
+
+function handleMouseOver() {
+  isHoverCard.value = true;
+}
+
+function handleMouseLeave() {
+  isHoverCard.value = false;
+}
+</script>
+
+<style></style>
--- frontend_vue/src/views/ComponentPreview.vue
+++ frontend_vue/src/views/ComponentPreview.vue
@@ -1,52 +1,12 @@
 <template>
   <div>
     <hr class="my-24" />
-    <h1>Checkbox input</h1>
-    <div class="flex flex-col gap-16 mt-24 mb-32">
-      <BaseInputCheckbox
-        id="pizza"
-        v-model="checkBoxValue"
-        label="Confirming my love for pizza 🍕 "
-        class="text-sm"
-      />
-      <div class="mt-8">Checkbox value: {{ checkBoxValue }}</div>
-      <BaseInputCheckbox
-        id="tooltip-checkbox"
-        v-model="checkBoxTooltipValue"
-        label="I have a tooltip "
-        class="text-sm"
-        tooltip-content="Boo!"
-      />
-      <BaseInputCheckbox
-        id="disabled"
-        v-model="checkBoxDisabledValue"
-        :disabled="true"
-        label="Disabled checkbox"
-        class="text-sm"
-      />
-    </div>
+    <h1>Asset Cards</h1>
+    <h2>Grid View</h2>
+    <AssetCard />
+    <h2>List View</h2>
   </div>
   <div>
-    <hr class="my-24" />
-    <h1>Step Counter</h1>
-    <div class="flex flex-col gap-16 mt-24 mb-32">
-      <BaseStepCounter
-        :steps="[
-          { label: 'Very long name' },
-          { label: 'Example 2' },
-          { label: 'Example 3' },
-          { label: 'Example long name' },
-          { label: 'Example 5' },
-        ]"
-        :current-step="currentStep"
-        @handle-step-click="(e) => (currentStep = e)"
-      />
-      <div class="flex gap-24 mt-16">
-        Current step: {{ currentStep }}
-        <button @click="currentStep--">Prev Step</button>
-        <button @click="currentStep++">Next Step</button>
-      </div>
-    </div>
     <hr class="my-24" />
     <h1>Image Select</h1>
     <div class="flex flex-col gap-16 mt-24 mb-32">
@@ -425,6 +385,7 @@ import BannerDeviceCanarytools from '@/components/ui/BannerDeviceCanarytools.vue
 import BannerBirdCanarytools from '@/components/ui/BannerBirdCanarytools.vue';
 import BannerTextCanarytools from '@/components/ui/BannerTextCanarytools.vue';
 import getImageUrl from '@/utils/getImageUrl';
+import AssetCard from '@/components/tokens/aws_infra/plan_generator/AssetCard.vue';
 
 const { open } = useModal({
   component: ModalToken,
