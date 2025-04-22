735704507d035d79fcf73671c7fb2089e23748a6
Vittoria Toso <vittoria@thinkst.com>
2025-04-22 23:23:07 +0200

    Add button to add new asset

---
+++ frontend_vue/src/components/tokens/aws_infra/plan_generator/ButtonAddAsset.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    class="border group bg-white rounded-2xl top-[0px] border-grey-200 duration-100 ease-in-out hover:border-green-600 focus:border-green-600"
+    @click="emits('add-asset')"
+  >
+    <font-awesome-icon
+      aria-hidden="true"
+      icon="plus"
+      class="group-hover:text-green-600 group-focus:text-green-600 text-grey-300"
+    ></font-awesome-icon>
+    <span class="sr-only">Add New {{ assetType }}</span>
+  </button>
+</template>
+
+<script lang="ts" setup>
+const emits = defineEmits(['add-asset']);
+
+defineProps<{
+  assetType: string;
+}>();
+</script>
