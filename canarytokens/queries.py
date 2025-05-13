4cefffaa3acfbbd731eb49c68f3782ffb968d42d
Lisa van Staden <lisa@thinkst.com>
2025-05-13 13:25:16 +0200

    Add pre shared auth fix z

--- canarytokens/queries.py
+++ canarytokens/queries.py
@@ -72,6 +72,8 @@ def get_canarydrop(canarytoken: tokens.Canarytoken) -> Optional[cand.Canarydrop]
         canarydrop["key_exposed_details"] = json.loads(
             canarydrop.pop("key_exposed_details")
         )
+    if "aws_infra_stage" in canarydrop:
+        canarydrop["aws_infra_stage"] = int(canarydrop["aws_infra_stage"])
 
     canarydrop["canarytoken"] = canarytoken
     try:
