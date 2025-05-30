e5c969c9f679b513e5af74f48dcba57ff8ccbc60
Frontend Github Action <max@thinkst.com>
2025-05-30 09:57:23 +0000

    Add Frontend Dist

---
+++ dist/assets/GenerateTokenForm-BGcjBa3a.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./GenerateTokenSettingsNotifications.vue_vue_type_script_setup_true_lang-CB-d__AF.js";import{d as o,a as r,b as a}from"./index-DJaz34Rt.js";const s=o({__name:"GenerateTokenForm",setup(n){return(m,t)=>(r(),a(e,{"memo-helper-example":"SVN token on REPO01/ourmainrepo"}))}});export{s as default};
---
+++ dist/assets/GenerateTokenForm-LXUnjFB5.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./GenerateTokenSettingsNotifications.vue_vue_type_script_setup_true_lang-CB-d__AF.js";import{d as o,a as r,b as a}from"./index-DJaz34Rt.js";const c=o({__name:"GenerateTokenForm",setup(t){return(n,s)=>(r(),a(e,{"memo-helper-example":" Hostname in /etc/hosts of server X"}))}});export{c as default};
