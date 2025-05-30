c8d74175d9622eed8150d89227d5f1b345726ad7
Vittoria Toso <vittoria@thinkst.com>
2025-05-30 10:36:11 +0200

    update tests

--- frontend_vue/src/utils/incidentDetailsService.spec.ts
+++ frontend_vue/src/utils/incidentDetailsService.spec.ts
@@ -99,6 +99,31 @@ describe('incidentDetailsService', () => {
     );
   });
 
+  it('should handle coords for PWA', () => {
+    const pwaAlert: HitsType = {
+      ...hitAlert,
+      token_type: 'pwa',
+      location: {
+        //@ts-ignore
+        coords: {
+          accuracy: 14.871000289916992,
+          altitude: 564.7999877929688,
+          altitudeAccuracy: null,
+          heading: null,
+          latitude: 18.6617495,
+          longitude: 73.708745,
+          speed: null,
+        },
+        timestamp: 1723125410760,
+      },
+    };
+
+    expect(incidentDetailsService(pwaAlert, 'pwa')).toHaveProperty(
+      'basic_info.location.accuracy',
+      14.871000289916992
+    );
+  });
+
   it('should show no tor relay for Credit Card Canarytoken', () => {
     const alert: HitsType = {
       time_of_hit: 1748445301.878416,
