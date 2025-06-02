4be21a627d8595d02ded7774707dfe394961ec02
Frontend Github Action <max@thinkst.com>
2025-06-02 13:29:05 +0000

    Add Frontend Dist

---
+++ dist/assets/GenerateTokenForm-f1Gtz071.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./GenerateTokenSettingsNotifications.vue_vue_type_script_setup_true_lang-DSmmD_Lf.js";import{d as o,a as r,b as a}from"./index-BmZPElBz.js";const c=o({__name:"GenerateTokenForm",setup(n){return(t,m)=>(r(),a(e,{"memo-helper-example":" Place where log line is inserted"}))}});export{c as default};
