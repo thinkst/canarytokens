7b81b651b811c25ceb1f22e084e7007d8f6bf6fa
Emmanuel Pitswane <emmanuel@thinkst.com>
2025-06-03 11:22:02 +0200

    Improve token info text

--- frontend_vue/src/components/tokens/web_image/ActivatedToken.vue
+++ frontend_vue/src/components/tokens/web_image/ActivatedToken.vue
@@ -6,8 +6,7 @@
   <base-message-box
     class="mt-24"
     variant="info"
-    :message="`If this URL is requested as an image (e.g. <img src=''>), or if the request includes an Accept header that allows an image response, your custom image will be served. If you've disable your custom image on this Canarytoken's management page, then a 1x1 image is served instead.
-    If you've enabled the browser scanner on this Canarytoken's management page, and the request includes an Accept header that allows HTML but not images, a blank HTML page containing fingerprinting JavaScript will be served. If the browser scanner is disabled, a HTML page displaying Earth will be returned.`"
+    :message="`If this URL is requested as an image, your custom image will be served. If you've disabled your custom image on this Canarytoken's management page, then a 1x1 image is served instead.`"
     text-link="More tips?"
     @click="() => $emit('howToUse')"
   />
