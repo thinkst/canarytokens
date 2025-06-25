d230ef80f60c91825b516d04c3a77ce73a317aa9
Vittoria Toso <vittoria@thinkst.com>
2025-06-25 17:54:20 +0200

    fix first step

---
+++ frontend_vue/src/components/tokens/aws_infra/token_setup_steps/ModalInfoSnippet.vue
@@ -0,0 +1,35 @@
+<template>
+  <BaseModal
+    title="What's this snippet doing?"
+    :has-close-button="true"
+    :content-class="`items-stretch`"
+  >
+    <template #default>
+      <div>
+        <BaseBulletList
+          :list="infoList"
+          class="mt-24 wrap-code"
+        />
+      </div>
+    </template>
+    <template #footer>
+      <BaseButton
+        variant="primary"
+        @click="closeModal"
+        >All clear!</BaseButton
+      >
+    </template>
+  </BaseModal>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  closeModal: () => void;
+}>();
+
+const infoList = [
+  'This step grants Canarytokens.org temporary, read-only access to list names of various AWS resources (S3 buckets, SQS queues, SSM parameters, Secrets Manager secrets, DynamoDB tables, and IAM roles) to create a customized infrastructure plan.',
+  'This access is automatically revoked by Canarytokens.org after plan creation.',
+  'You will be provided with cleanup instructions at the end of the wizard to remove the associated AWS policy, attachment, and role.',
+];
+</script>
