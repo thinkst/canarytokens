63651ef0259d20aa43f2107564bf404d5aaa1df0
Vittoria Toso <vittoria@thinkst.com>
2025-07-08 18:38:53 +0200

    restore wizard

--- frontend_vue/src/components/tokens/aws_infra/TokenSetupWizard.vue
+++ frontend_vue/src/components/tokens/aws_infra/TokenSetupWizard.vue
@@ -74,7 +74,7 @@ const props = defineProps<{
 }>();
 
 const router = useRouter();
-const currentStep = ref(2);
+const currentStep = ref(1);
 const sharedData = ref(Array(3).fill({}));
 const stepComponents = ref<
   Record<number, ReturnType<typeof defineAsyncComponent>>
