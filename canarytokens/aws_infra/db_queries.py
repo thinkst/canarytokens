1a45052a69a1d2fd6fbae96b2d94e4f9f923169e
Dev Dua <dev@thinkst.com>
2025-07-22 15:03:28 +0530

    Expire inventoried assets

---
+++ canarytokens/aws_infra/db_queries.py
@@ -0,0 +1,40 @@
+from canarytokens.canarydrop import Canarydrop
+from canarytokens.redismanager import DB
+from datetime import timedelta
+
+import json
+
+INVENTORY_EXPIRY = timedelta(hours=3).seconds  # 3 hours
+
+
+def _inventory_key(canarydrop: Canarydrop):
+    return f"{canarydrop.canarytoken.value()}_aws_inventoried_assets"
+
+
+def get_current_assets(canarydrop: Canarydrop) -> dict:
+    """
+    Retrieve the current assets inventoried for a given canarydrop.
+    Returns an empty dict if no assets are found.
+    :param canarydrop: The canarydrop instance for which to retrieve assets.
+    """
+    with DB.get_db() as r:
+        return json.loads(r.get(_inventory_key(canarydrop))) or {}
+
+
+def save_current_assets(canarydrop: Canarydrop, assets: dict) -> None:
+    """
+    Save the current assets inventoried for a given canarydrop.
+    :param canarydrop: The canarydrop instance for which to save assets.
+    :param assets: The inventoried assets to save.
+    """
+    with DB.get_db() as r:
+        r.set(_inventory_key(canarydrop), json.dumps(assets), ex=INVENTORY_EXPIRY)
+
+
+def delete_current_assets(canarydrop: Canarydrop) -> None:
+    """
+    Delete the current assets inventoried for a given canarydrop.
+    :param canarydrop: The canarydrop instance for which to delete assets.
+    """
+    with DB.get_db() as r:
+        r.delete(_inventory_key(canarydrop))
