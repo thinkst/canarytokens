e18ce1f78b8cb7d5491ce0adc21cdea59173cfb4
Lisa van Staden <lisa@thinkst.com>
2025-07-28 17:37:39 +0200

    temp fix

--- canarytokens/models.py
+++ canarytokens/models.py
@@ -2971,6 +2971,7 @@ class AWSInfraSetupIngestionReceivedResponse(BaseModel):
     message: str = ""
     handle: str
     terraform_module_snippet: dict = None
+    role_cleanup_commands: dict = None
 
 
 class AWSInfraTeardownReceivedResponse(BaseModel):
