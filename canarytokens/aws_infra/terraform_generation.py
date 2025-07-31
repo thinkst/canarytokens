cdb5f077e5d02f0670dfd2c3d446243875d4fa81
Lisa van Staden <lisa@thinkst.com>
2025-07-31 14:38:56 +0200

    fix plan assets

--- canarytokens/aws_infra/terraform_generation.py
+++ canarytokens/aws_infra/terraform_generation.py
@@ -50,7 +50,7 @@ def generate_tf_variables(canarydrop: Canarydrop, plan: dict) -> dict:
 
 
 def _add_s3_buckets(tf_variables, plan):
-    for bucket in plan["assets"].get(AWSInfraAssetType.S3_BUCKET, []):
+    for bucket in plan.get(AWSInfraAssetType.S3_BUCKET, []):
         tf_variables[Variable.S3_BUCKET_NAMES].append(
             bucket[AWSInfraAssetField.BUCKET_NAME]
         )
@@ -65,14 +65,14 @@ def _add_s3_buckets(tf_variables, plan):
 
 
 def _add_sqs_queues(tf_variables, plan):
-    for queue in plan["assets"].get(AWSInfraAssetType.SQS_QUEUE, []):
+    for queue in plan.get(AWSInfraAssetType.SQS_QUEUE, []):
         tf_variables[Variable.SQS_QUEUES].append(
             queue[AWSInfraAssetField.SQS_QUEUE_NAME]
         )
 
 
 def _add_ssm_parameters(tf_variables, plan):
-    for param in plan["assets"].get(AWSInfraAssetType.SSM_PARAMETER, []):
+    for param in plan.get(AWSInfraAssetType.SSM_PARAMETER, []):
         tf_variables[Variable.SSM_PARAMETERS].append(
             {
                 "name": param[AWSInfraAssetField.SSM_PARAMETER_NAME],
@@ -82,14 +82,14 @@ def _add_ssm_parameters(tf_variables, plan):
 
 
 def _add_secrets(tf_variables, plan):
-    for secret in plan["assets"].get(AWSInfraAssetType.SECRETS_MANAGER_SECRET, []):
+    for secret in plan.get(AWSInfraAssetType.SECRETS_MANAGER_SECRET, []):
         tf_variables[Variable.SECRETS].append(
             secret[AWSInfraAssetField.SECRET_NAME],
         )
 
 
 def _add_dynamodb_tables(tf_variables, plan):
-    for table in plan["assets"].get(AWSInfraAssetType.DYNAMO_DB_TABLE, []):
+    for table in plan.get(AWSInfraAssetType.DYNAMO_DB_TABLE, []):
         tf_variables[Variable.TABLES].append(table[AWSInfraAssetField.TABLE_NAME])
         for item in table.get("table_items", []):
             tf_variables[Variable.TABLE_ITEMS].append(
