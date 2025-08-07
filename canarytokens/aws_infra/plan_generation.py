39013e00f113b0456bb5045487c969e86da82b84
Lisa van Staden <lisa@thinkst.com>
2025-08-07 16:42:57 +0200

    update limits

--- canarytokens/aws_infra/plan_generation.py
+++ canarytokens/aws_infra/plan_generation.py
@@ -241,17 +241,17 @@ class AssetTypeConfig:
 
 _ASSET_TYPE_CONFIG = {
     AWSInfraAssetType.S3_BUCKET: AssetTypeConfig(
-        5, AWSInfraAssetField.BUCKET_NAME, AWSInfraAssetField.OBJECTS, 20
+        10, AWSInfraAssetField.BUCKET_NAME, AWSInfraAssetField.OBJECTS, 20
     ),
-    AWSInfraAssetType.SQS_QUEUE: AssetTypeConfig(10, AWSInfraAssetField.SQS_QUEUE_NAME),
+    AWSInfraAssetType.SQS_QUEUE: AssetTypeConfig(5, AWSInfraAssetField.SQS_QUEUE_NAME),
     AWSInfraAssetType.SSM_PARAMETER: AssetTypeConfig(
-        5, AWSInfraAssetField.SSM_PARAMETER_NAME
+        10, AWSInfraAssetField.SSM_PARAMETER_NAME
     ),
     AWSInfraAssetType.SECRETS_MANAGER_SECRET: AssetTypeConfig(
         5, AWSInfraAssetField.SECRET_NAME
     ),
     AWSInfraAssetType.DYNAMO_DB_TABLE: AssetTypeConfig(
-        5, AWSInfraAssetField.TABLE_NAME, AWSInfraAssetField.TABLE_ITEMS, 20
+        10, AWSInfraAssetField.TABLE_NAME, AWSInfraAssetField.TABLE_ITEMS, 20
     ),
 }
 
