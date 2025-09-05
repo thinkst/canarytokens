0a4d101e3947b4ee0462f472f2f8056e3ed6f21e
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2025-09-05 16:27:11 +0200

    add intro (#759)

--- frontend_vue/src/components/tokens/aws_infra/GenerateTokenCustom.vue
+++ frontend_vue/src/components/tokens/aws_infra/GenerateTokenCustom.vue
@@ -1,9 +1,17 @@
 <template>
-  <TokenSetupWizard />
+  <GenerateTokenIntro
+    v-if="showIntro"
+    @start-token-setup="showIntro = false"
+  />
+  <TokenSetupWizard v-else />
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import GenerateTokenIntro from '@/components/tokens/aws_infra/GenerateTokenIntro.vue';
 import TokenSetupWizard from '@/components/tokens/aws_infra/TokenSetupWizard.vue';
+
+const showIntro = ref(true);
 </script>
 
 <style></style>
