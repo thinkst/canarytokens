ebb807c36e419ff587d4707705d5b4124d0dbed3
Lisa van Staden <lisa@thinkst.com>
2025-10-24 16:02:00 +0200

    custom installs

--- .devcontainer/custom-installs.sh
+++ .devcontainer/custom-installs.sh
@@ -3,4 +3,4 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B7B3B788A8D3785C
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.22-1_all.deb
 DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.22-1_all.deb
 apt update
-DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-client
+DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-clientgit
