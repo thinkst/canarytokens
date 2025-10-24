362c21824db302ff9887aa6314942af23f86ceaa
Lisa van Staden <lisa@thinkst.com>
2025-10-24 17:06:27 +0000

    update customs

--- .devcontainer/custom-installs.sh
+++ .devcontainer/custom-installs.sh
@@ -1,6 +1,7 @@
-# Install mysql (default repos are broken for buster)
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B7B3B788A8D3785C
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.22-1_all.deb
-DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.22-1_all.deb
-apt update
-DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-client
+# Install MySQL client (using default-mysql-client for Debian 11+)
+DEBIAN_FRONTEND=noninteractive apt-get install -y \
+    default-mysql-client \
+    libzbar0 \
+    osslsigncode \
+    dnsutils \
+    subversion
