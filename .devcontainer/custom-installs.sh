2bd913c0fbc778470eebf2b1a80bad31991f94f2
Lisa van Staden <lisa@thinkst.com>
2025-10-24 17:37:52 +0000

    cleanup 2

--- .devcontainer/custom-installs.sh
+++ .devcontainer/custom-installs.sh
@@ -1,5 +1,5 @@
 # Install MySQL client (using default-mysql-client for Debian 11+)
-DEBIAN_FRONTEND=noninteractive apt-get install -y \
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends\
     default-mysql-client \
     libzbar0 \
     osslsigncode \
