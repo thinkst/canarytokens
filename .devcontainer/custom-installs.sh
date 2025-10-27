573fa6ab8b0a4b42a72182b72e007282827f1f36
Lisa van Staden <lisa@thinkst.com>
2025-10-27 14:06:45 +0000

    remove custom install

--- .devcontainer/custom-installs.sh
+++
@@ -1,7 +0,0 @@
-# Install MySQL client (using default-mysql-client for Debian 11+)
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends\
-    default-mysql-client \
-    libzbar0 \
-    osslsigncode \
-    dnsutils \
-    subversion
