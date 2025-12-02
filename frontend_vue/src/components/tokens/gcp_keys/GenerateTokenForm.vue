8fb0938f273f0ae7b47795deddbb8d6801528c50
Jason Bissict <jason@thinkst.com>
2025-12-02 09:26:15 +0200

    Add missing gcp files

---
+++ frontend_vue/src/components/tokens/gcp_keys/GenerateTokenForm.vue
@@ -0,0 +1,9 @@
+<template>
+  <GenerateTokenSettingsNotifications
+    memo-helper-example="GCP keys placed on Jim's laptop"
+  />
+</template>
+
+<script setup lang="ts">
+import GenerateTokenSettingsNotifications from '@/components/ui/GenerateTokenSettingsNotifications.vue';
+</script>
---
+++ frontend_vue/src/components/tokens/gcp_keys/TokenDisplay.vue
@@ -0,0 +1,23 @@
+<template>
+  <base-content-block
+    lang="json"
+    label="GCP token"
+    :text="GCPKeyCode"
+    :copyContent="true"
+    :icon-name="'lock'"
+  ></base-content-block>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+type GCPKeysDataType = {
+  gcp_keyfile: string;
+  gcp_service_account_email: string;
+};
+
+const props = defineProps<{
+  tokenData: GCPKeysDataType;
+}>();
+const GCPKeyCode = ref(`${props.tokenData.gcp_keyfile}`);
+</script>
