2e20c835b003c7dee2068eb39f7bddcc82cff4f5
gjcthinkst <135841050+gjcthinkst@users.noreply.github.com>
2026-01-22 15:00:06 +0200

    Force no SSL for mysql dump token (#825)

--- frontend_vue/src/components/tokens/my_sql/generateMysqlToken.ts
+++ frontend_vue/src/components/tokens/my_sql/generateMysqlToken.ts
@@ -3,12 +3,12 @@ export default function generateMysqlToken(
   token: string,
   encoded: boolean
 ) {
-  const decodedCode = `SET @bb = CONCAT(\"CHANGE REPLICATION SOURCE TO SOURCE_PASSWORD='my-secret-pw', SOURCE_RETRY_COUNT=1, SOURCE_PORT=3306, SOURCE_HOST='${hostname}', SOURCE_USER='${token}\", @@lc_time_names, @@hostname, \"';\")`;
+  const decodedCode = `SET @bb = CONCAT(\"CHANGE REPLICATION SOURCE TO SOURCE_PASSWORD='my-secret-pw', SOURCE_RETRY_COUNT=1, SOURCE_PORT=3306, SOURCE_HOST='${hostname}', SOURCE_SSL=0, SOURCE_USER='${token}\", @@lc_time_names, @@hostname, \"';\")`;
   const encodedCode = btoa(
-    `SET @bb = CONCAT(\"CHANGE REPLICATION SOURCE TO SOURCE_PASSWORD='my-secret-pw', SOURCE_RETRY_COUNT=1, SOURCE_PORT=3306, SOURCE_HOST='${hostname}', SOURCE_USER='${token}\", @@lc_time_names, @@hostname, \"';\"`
+    `SET @bb = CONCAT(\"CHANGE REPLICATION SOURCE TO SOURCE_PASSWORD='my-secret-pw', SOURCE_RETRY_COUNT=1, SOURCE_PORT=3306, SOURCE_HOST='${hostname}', SOURCE_SSL=0, SOURCE_USER='${token}\", @@lc_time_names, @@hostname, \"';\"`
   );
   const code = encoded
-    ? `SET @b = ${encodedCode};
+    ? `SET @b = '${encodedCode}';
 SET @s2 = FROM_BASE64(@b);
 PREPARE stmt1 FROM @s2;
 EXECUTE stmt1;
