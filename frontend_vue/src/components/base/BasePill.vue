3c4e1db357c5a42013f9c19999269cfeea194764
Lisa van Staden <lisa@thinkst.com>
2026-02-23 11:16:31 +0200

    fix pill classes

--- frontend_vue/src/components/base/BasePill.vue
+++ frontend_vue/src/components/base/BasePill.vue
@@ -1,6 +1,7 @@
 <template>
-    <p class="text-xs rounded-xl px-16 py-4 inline-block text-nowrap"
-        :class="`bg-${backgroundColour} text-${textColour}`"
+    <p
+        class="text-xs rounded-xl px-16 py-4 inline-block text-nowrap"
+        :class="getColourClasses(props.colour)"
     >
         <slot></slot>
     </p>
@@ -9,13 +10,20 @@
 <script setup lang="ts">
 
 const props = defineProps({
-  backgroundColour: {
+  colour: {
     type: String,
     default: 'grey'
   },
-  textColour: {
-    type: String,
-    default: 'white'
-  }
 });
+
+function getColourClasses(colour: string): string {
+  switch (colour) {
+    case 'grey':
+      return 'bg-grey-400 text-white';
+    // add more colours here as needed
+    default:
+      return 'bg-grey-400 text-white';
+  }
+}
+
 </script>
