ad63c65dc13c135c52999ce2fdbc637f5a1260b1
ivan <218958954+thinkst-ivan@users.noreply.github.com>
2026-03-10 14:41:49 +0200

    Remove color from bools
    Remove color from bools

--- frontend_vue/src/components/ui/IncidentDetailsListItem.vue
+++ frontend_vue/src/components/ui/IncidentDetailsListItem.vue
@@ -1,7 +1,7 @@
 <template>
   <li>
     <span class="block text-xs uppercase text-grey-500">{{ label }}</span>
-    <span :class="highlightBoolean(value)">{{ value }}</span>
+    <span>{{ value }}</span>
   </li>
 </template>
 
@@ -10,13 +10,4 @@ defineProps<{
   label?: string | number;
   value: string | boolean | number;
 }>();
-
-/**
- * Returns a CSS class name based on the boolean value of the input parameter.
- */
-function highlightBoolean(val: string | number | boolean) {
-  if (typeof val === 'boolean') {
-    return val ? 'text-green' : 'text-red';
-  }
-}
 </script>
