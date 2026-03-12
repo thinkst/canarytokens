25c9612c4c3b73257277671512e774972821478f
Heather Hughes <heather@thinkst.com>
2026-03-12 14:41:38 +0200

    Automated testing - created

--- frontend_vue/tests/create-token.spec.ts
+++ frontend_vue/tests/create-token.spec.ts
@@ -1,37 +1,108 @@
 import { test, expect } from '@playwright/test';
 import { waitForApiSuccess } from './utils/common';
 import { takeScreenshot } from './utils/screenshots';
-// import { tokenServices } from '../src/utils/tokenServices';
+import { tokenServices } from '../src/utils/tokenServices';
+import { TOKENS_TYPE } from '../src/components/constants';
+import path from 'path';
+
+const TOKENS_TO_SKIP = [
+  TOKENS_TYPE.CREDIT_CARD_V2,
+  TOKENS_TYPE.AWS_INFRA,
+  TOKENS_TYPE.AZURE_ID, //This can be removed once we fix the functionality in honeypdfs 
+  TOKENS_TYPE.SQL_SERVER //Mission to set up - later problem 
+]
 
 test.describe('Create Token', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
   });
 
-  test('Can create Web bug Canarytoken', async ({ page }) => {
-    const webBugCard = page.getByText('Web bug');
-    await expect(webBugCard).toBeVisible();
-    await webBugCard.click();
+  Object.keys(tokenServices).forEach((service) => {
+    if (TOKENS_TO_SKIP.includes(service)) {
+      console.warn(`Skipping tests for ${service} Canarytoken`);
+      return;
+    }
+    const tokenObj = tokenServices[service as keyof typeof tokenServices];
+    const serviceName = tokenObj.label;
 
-    await expect(page.getByRole('heading', { name: 'Create Web bug Token' })).toBeVisible();
-    await expect(page.getByText('Mail me here when the alert fires')).toBeVisible();
-    await page.locator('#email').fill('test@example.com');
-    await expect(page.getByText('Remind me of this when the alert fires')).toBeVisible();
-    await page.locator('#memo').fill('This is a reminder for the Web bug Canarytoken alert.');
-    await takeScreenshot(page, 'web-bug/token-form');
+    test(`Can create ${tokenObj.label} Canarytoken`, async ({ page }) => {
+      const tokenCard = page.getByText(serviceName).first();
+      await expect(tokenCard).toBeVisible();
+      await tokenCard.click();
 
-    const createBtn = page.getByRole('button', { name: 'Create Canarytoken' });
-    await expect(createBtn).toBeVisible();
-    await createBtn.click();
+      if (service === TOKENS_TYPE.PWA) {
+        await page.locator('label[for="absa"]').click();
+        await expect(page.locator('input#absa')).toBeChecked();
+      }
 
-    await waitForApiSuccess({
-      page,
-      url: '/d3aece8093b71007b5ccfedad91ebb11/generate',
-      callback: async () => {
-        await expect(page.getByText('Your Web bug Canarytoken is active!')).toBeVisible();
-        await expect(page.getByRole('button', { name: 'Manage Canarytoken' })).toBeVisible();
-        await takeScreenshot(page, 'web-bug/token-created')
-      },
-    })
+      if (service === TOKENS_TYPE.FAST_REDIRECT || service === TOKENS_TYPE.SLOW_REDIRECT) {
+        page.getByRole('textbox', { name: 'Redirect URL' }).fill('https://example.com');
+      }
+
+      if (service === TOKENS_TYPE.CLONED_WEBSITE || service === TOKENS_TYPE.CSS_CLONED_SITE) {
+        page.getByRole('textbox', { name: 'Domain of protected website' }).fill('example.com');
+      }
+
+      if (service === TOKENS_TYPE.SENSITIVE_CMD) {
+        page.getByRole('textbox', { name: 'Name of the process to monitor' }).fill('whoami.exe');
+      }
+
+      if (service === TOKENS_TYPE.WINDOWS_FAKE_FS) {
+        page.getByRole('textbox', { name: 'Where will this directory be placed?' }).fill('C:\\Desktop\\FakeFS');
+        const industryInput = page.locator('input.vs__search[placeholder="Choose an Industry/Sector"]');
+        await industryInput.click();
+        await page.getByRole('option', { name: 'Personal Finances' }).click();
+      }
+
+      if (service === TOKENS_TYPE.WEBDAV) {
+        const WebDavInput = page.locator('input.vs__search[placeholder="Select dummy folder content"]');
+        await WebDavInput.click();
+        await page.getByRole('option', { name: 'Cyber Security' }).click();
+      }
+
+      if (service === TOKENS_TYPE.IDP_APP) {
+        const IDPAppInput = page.locator('input.vs__search[placeholder="Select an app"]');
+        await IDPAppInput.click();
+        await page.getByRole('option', { name: 'Gmail' }).click();
+      }
+
+
+      if (service === TOKENS_TYPE.WEB_IMAGE) {
+        const nonImageFile = path.join(__dirname, './upload/WebImage.jpg');
+        await page.getByTestId('doc-msword-document').setInputFiles(nonImageFile);
+      }
+
+      if (service === TOKENS_TYPE.CUSTOM_EXE){
+        const exeFilePath = path.join(__dirname, './upload/CustomExe.exe');
+        await page.getByTestId('custom-exe-file').setInputFiles(exeFilePath);
+      }
+      
+      //This feature does not work on honeypdfs but the below can be uncommented once we fix the functionality
+      // if (service === TOKENS_TYPE.AZURE_ID){
+      //   page.getByRole('textbox', { name: 'Azure ID certificate name' }).fill('testingtokens.pem');
+      // }
+
+      await expect(page.getByText('Mail me here when the alert fires')).toBeVisible();
+      await page.locator('#email').fill('test@example.com');
+      await expect(page.getByText('Remind me of this when the alert fires')).toBeVisible();
+      await page.locator('#memo').fill(`This is a reminder for the ${serviceName} Canarytoken alert.`);
+      await takeScreenshot(page, `${serviceName.toLowerCase()}/token-form`);
+
+      const createBtn = page.getByRole('button', { name: 'Create Canarytoken' });
+      await expect(createBtn).toBeVisible();
+      await createBtn.click();
+
+      await waitForApiSuccess({
+        page,
+        url: '/d3aece8093b71007b5ccfedad91ebb11/generate',
+        callback: async () => {
+          await expect(page.getByText(`Your ${serviceName} Canarytoken is active!`)).toBeVisible();
+          await expect(page.getByRole('button', { name: 'Manage Canarytoken' })).toBeVisible();
+          await takeScreenshot(page, `${serviceName.toLowerCase()}/token-created`)
+        },
+      })
+    });
+
   });
+
 });
