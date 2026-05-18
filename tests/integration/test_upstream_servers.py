58e8512993917771c338f0e95171af1080fd1a52
Erin H <erin@thinkst.com>
2026-05-18 13:23:12 +0200

    Improve tor exit node list request behavior during tests

---
+++ tests/integration/test_upstream_servers.py
@@ -0,0 +1,19 @@
+
+import ipaddress
+from canarytokens import queries
+from twisted.internet.defer import inlineCallbacks
+
+@inlineCallbacks
+def test_upstream_tor_exit_nodes_list(setup_db):
+    # Test the real upstream exactly once to not overload it with requests
+    # originating from CI, but still be alerted if it stops working
+
+    queries.clear_tor_exit_nodes()
+    assert queries.is_tor_relay('8.8.8.8') is None
+
+    ips = yield queries.update_tor_exit_nodes()
+    assert len(ips) > 10
+    for ip in ips[:10]:
+        assert queries.is_tor_relay(ip) is True
+
+    assert queries.is_tor_relay('8.8.8.8') is False
