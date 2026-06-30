55af6bac8277caea4fe7426b71e7bf7438b2359a
Vittoria Toso <vittoria@thinkst.com>
2026-06-30 15:13:34 +0200

    update README

--- frontend_vue/src/components/constants.ts
+++ frontend_vue/src/components/constants.ts
@@ -5,8 +5,6 @@ TOKENS_TYPE defines a set of token types that are used in various parts of the a
 When you add a new token inside @/components/tokens,
 name the new folder as the token_type
 
-TODO: if possible, we might add a eslint rule
-to check if a folder name matches a TOKENS_TYPE type
 */
 
 export const TOKENS_TYPE = {
