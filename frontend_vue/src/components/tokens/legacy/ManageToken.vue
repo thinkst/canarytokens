65d2d0e3cd9ada31fefdf5ce771dd7c2d0108a25
vittoriaThinkst <126554007+vittoriaThinkst@users.noreply.github.com>
2026-07-02 17:12:31 +0200

    Add Support for “Legacy” token (#953)

---
+++ frontend_vue/src/components/tokens/legacy/ManageToken.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="!props.tokenBackendResponse">Error loading</div>
+<BaseMessageBox variant="warning">
+  This is a legacy token that was created prior to 2017.<br/>
+  These tokens alert but <strong class="font-bold">miss some meta-data</strong> that helps during incidents.<br/>
+  We <strong class="font-bold">recommend replacing</strong> this older token with a current alternative.
+</BaseMessageBox>
+</template>
+
+<script lang="ts" setup>
+import type { ManageTokenBackendType } from '@/components/tokens/types.ts';
+
+const props = defineProps<{
+  tokenBackendResponse: ManageTokenBackendType;
+}>();
+
+
+</script>
